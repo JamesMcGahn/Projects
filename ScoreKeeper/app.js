dde001a5576a6822512db3e670d1254f496eaca5
almag3st <jmcgahn@gmail.com>
2021-03-12 11:07:02 -0500

    fixed score logic to game till selected score, added tracking match count and displaying matches wins

--- ScoreKeeper/app.js
+++ ScoreKeeper/app.js
@@ -1,45 +1,54 @@
 let player1Total = 0;
 let player2Total = 0;
 let game = 0;
+let match = 1;
+let player1Wins = 0;
+let player2Wins = 0;
 
 const scoreKeeper = {
     scoreCard: function (e) {
         e.target.id === "player1" ? player1Total++ : player2Total++;
 
-        scoreKeeper.updateBoard();
         scoreKeeper.isWinner();
+        scoreKeeper.updateBoard();
     },
     updateBoard: function () {
         player1Board.innerText = `${player1Total}`;
         player2Board.innerText = `${player2Total}`;
-        if (player1Total - player2Total > 4 || player2Total - player1Total > 4) {
-            msgBoard.innerText = "Lets Go!!!"
-        } else {
+        if (player1Total + player2Total !== game) {
             msgBoard.innerText = "VS."
         }
-
+        matchBoard.innerHTML = `<div> <h2> Match ${match}</h2> <p>Player 1 -- ${player1Wins} -- Player 2 ${player2Wins}</p> </div>`
     },
     gameTotal: function gameTotal(e) {
         game = parseInt(e.target.value);
+
+        player1.disabled = false;
+        player2.disabled = false;
+
     },
     isWinner: function () {
-        if ((player1Total + player2Total) === game) {
+        if (player1Total === game || player2Total === game) {
             if (player1Total > player2Total) {
                 console.log('player 1 wins');
                 player1.disabled = true;
                 player1.style.backgroundColor = "green";
                 player2.style.backgroundColor = "red";
                 player2.disabled = true;
-                msgBoard.innerText = "Player 1 Wins .......Maybe next try or next Year?"
+                msgBoard.innerText = "Player 1 Wins"
                 scoreKeeper.newGameBtn();
+                player1Wins++;
+                match++
             } else {
                 console.log('player 2 wins');
                 player1.disabled = true;
                 player2.disabled = true;
                 player2.style.backgroundColor = "green";
                 player1.style.backgroundColor = "red";
-                msgBoard.innerText = "Player 2 Wins .....You really want to try again?"
+                msgBoard.innerText = "Player 2 Wins"
                 scoreKeeper.newGameBtn();
+                player2Wins++;
+                match++
             }
         }
 
@@ -64,9 +73,11 @@ const scoreKeeper = {
         player1Total = 0;
         removeNewGameBtn = document.querySelector('#newgame');
         removeNewGameBtn.remove();
-        scoreKeeper.updateBoard();
+
         player2.style.backgroundColor = "rgb(116, 116, 255)";
         player1.style.backgroundColor = "rgb(116, 116, 255)";
+
+        scoreKeeper.updateBoard();
     }
 
 }
@@ -76,6 +87,7 @@ const playUntil = document.querySelector('#playTill');
 const player1Board = document.querySelector('#play1board');
 const player2Board = document.querySelector('#play2board');
 const msgBoard = document.querySelector('#msg');
+const matchBoard = document.querySelector('#matchcount');
 
 
 player1.addEventListener('click', scoreKeeper.scoreCard)
