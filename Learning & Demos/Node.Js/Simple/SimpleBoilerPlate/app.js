3f7fa562ee426d7c685b4aef40ef50d1bd26889e
almag3st <jmcgahn@gmail.com>
2021-03-14 15:22:16 -0400

    playing with node scripts

---
+++ Learning & Demos/Node.Js/Simple/SimpleBoilerPlate/app.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const folderName = process.argv[2] || 'Unnamed Project';
+const htmlFile = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>`;
+
+
+
+try {
+    fs.mkdirSync(folderName);
+    fs.writeFileSync(`${folderName}/index.html`, htmlFile, 'utf8');
+    fs.writeFileSync(`${folderName}/styles.css`, '');
+    fs.writeFileSync(`${folderName}/app.js`, '');
+} catch (e) {
+    console.log(e)
+};
+
+
+
+// fs.mkdir('/tmp/a/apple', { recursive: true }, (err) => {
+//     if (err) throw err;
+// });
