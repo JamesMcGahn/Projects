26c8e75faf88e147339cd1df69e5654ccccdae89
almag3st <jmcgahn@gmail.com>
2021-03-16 17:05:57 -0400

    added files & packages - connected to express

---
+++ Learning & Demos/Express.js/Demos/DemoMongooseExpress/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.listen(3012, () => {
+    console.log("listening on port 3012");
+})
