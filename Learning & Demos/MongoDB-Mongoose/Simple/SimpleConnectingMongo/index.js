79fdf38d3da429d0aadb4195cc60b79c55eadc2e
almag3st <jmcgahn@gmail.com>
2021-03-16 15:18:12 -0400

    mongoose schema

--- Learning & Demos/MongoDB-Mongoose/Simple/SimpleConnectingMongo/index.js
+++ Learning & Demos/MongoDB-Mongoose/Simple/SimpleConnectingMongo/index.js
@@ -6,3 +6,12 @@ mongoose.connect('mongodb://localhost:27017/movieApp', { useNewUrlParser: true, 
     .catch(err => {
         console.log("DB Connection Error", err);
     })
+
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
