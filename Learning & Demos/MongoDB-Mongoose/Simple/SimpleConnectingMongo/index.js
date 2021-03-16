0ee9638c7ed1c46e3f98475a4e84bb46e55c0388
almag3st <jmcgahn@gmail.com>
2021-03-15 22:36:41 -0400

    mongo connected

--- Learning & Demos/MongoDB-Mongoose/Simple/SimpleConnectingMongo/index.js
+++ Learning & Demos/MongoDB-Mongoose/Simple/SimpleConnectingMongo/index.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/movieApp', { useNewUrlParser: true, useUnifiedTopology: true });
-
-
+mongoose.connect('mongodb://localhost:27017/movieApp', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Connection Open")
+    })
+    .catch(err => {
+        console.log("DB Connection Error", err);
+    })
