74c7cbf8e40ebcd92c92ea58a142853c78230de6
almag3st <jmcgahn@gmail.com>
2021-03-16 18:46:47 -0400

    added route, and basic template forget products

--- Learning & Demos/Express.js/Demos/DemoMongooseExpress/index.js
+++ Learning & Demos/Express.js/Demos/DemoMongooseExpress/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/shopApp', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const Product = require('./models/product');
+
+mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("Connection Open")
     })
@@ -10,13 +13,17 @@ mongoose.connect('mongodb://localhost:27017/shopApp', { useNewUrlParser: true, u
         console.log("DB Connection Error", err);
     })
 
-
-
-
-
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+
+app.get('/products', async (req, res) => {
+    const products = await Product.find({});
+    console.log(products);
+    res.render('products/index', { products });
+})
+
+
 
 app.listen(3012, () => {
     console.log("listening on port 3012");
