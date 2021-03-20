6c6128689da4978910cd415bdbc1dc1b3092a2df
almag3st <jmcgahn@gmail.com>
2021-03-20 00:21:13 -0400

    added joi and schemas for validation

--- Projects From Tuts/YelpCamp/index.js
+++ Projects From Tuts/YelpCamp/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const { campgroundSchema } = require('./schemas.js')
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
 const catchAsync = require('./utils/catchAsync');
@@ -33,6 +34,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+    console.log(result);
+}
+
 app.get('/', (req, res) => {
     res.render('home');
 })
@@ -48,8 +60,8 @@ app.get('/campgrounds/new', (req, res) => {
 
 
 
-app.post('/campgrounds', catchAsync(async (req, res) => {
-    if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res) => {
+    //if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -67,7 +79,7 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     res.redirect('/campgrounds');
 }))
 
-app.put('/campgrounds/:id', catchAsync(async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`);
@@ -84,10 +96,9 @@ app.all('*', (req, res, next) => {
 })
 
 app.use((err, req, res, next) => {
-    const { statusCode = 500, message = 'Something went Wrong' } = err;
-    res.status(statusCode).render('error', { statusCode, message });
-
-    res.send('error')
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = "Something Went Wrong";
+    res.status(statusCode).render('error', { err });
 })
 
 app.listen(3020, (req, res) => {
