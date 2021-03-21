f742c04043982cabf96d72f54debb87b0748ab3a
almag3st <jmcgahn@gmail.com>
2021-03-21 17:22:14 -0400

    login route & form

--- Projects From Tuts/YelpCamp/routes/users.js
+++ Projects From Tuts/YelpCamp/routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
-
+const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 
 router.get('/register', (req, res) => {
@@ -20,6 +20,15 @@ router.post('/register', catchAsync(async (req, res) => {
         res.redirect('/register');
 
     }
-
 }))
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+})
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'Welcome Back');
+    res.redirect('/campgrounds');
+})
+
 module.exports = router;
