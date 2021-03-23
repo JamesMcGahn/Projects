7b7739a5e1827116b407a11cc9dbc253d488b356
almag3st <jmcgahn@gmail.com>
2021-03-23 02:16:45 -0400

    ready to deploy

--- Projects From Tuts/YelpCamp/index.js
+++ Projects From Tuts/YelpCamp/index.js
@@ -15,14 +15,20 @@ const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
+
+
+
+const MongoDBStore = require("connect-mongo");
+
+
 //Routes
 const userRoutes = require('./routes/users')
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
-
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
@@ -97,13 +103,24 @@ app.use(
 
 
 
-
+const secret = process.env.SECRET || 'needbettersecret';
 
 // milli secs in sec * secs in min * mins in hour * hours in day * days in week
 
+const store = new MongoDBStore({
+    mongoUrl: dbUrl,
+    secret,
+    touchAfter: 24 * 60 * 60
+});
+
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e)
+})
+
 const sessionConfig = {
+    store,
     name: 'session',
-    secret: 'needbettersecret',
+    secret: secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
