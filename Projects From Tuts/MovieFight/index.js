cfa9cbfb06cb5ee2e3860aa380af62fe78453bed
almag3st <jmcgahn@gmail.com>
2021-03-27 21:45:20 -0400

    added axios function, e listener on input ->  search

--- Projects From Tuts/MovieFight/index.js
+++ Projects From Tuts/MovieFight/index.js
@@ -1,1 +1,16 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+    const res = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: keys.API_KEY,
+            s: searchTerm,
+        }
+    })
+
+    console.log(res.data)
+};
+
+
+const input = document.querySelector('input');
+input.addEventListener('input', (event) => {
+    fetchData(event.target.value);
+})
