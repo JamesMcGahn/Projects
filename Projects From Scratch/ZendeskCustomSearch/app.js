a1bd0b0a6b588ceefdd9e481606ed7e2af939be0
almag3st <jmcgahn@gmail.com>
2021-03-28 05:42:29 -0400

    search bar added, debounce wrapper fn added, basic display of company search results

--- Projects From Scratch/ZendeskCustomSearch/app.js
+++ Projects From Scratch/ZendeskCustomSearch/app.js
@@ -9,9 +9,38 @@ const fetchData = async (searchTerm) => {
 
             }
         })
-        console.log(res.data.results)
+        return res.data;
     } catch (err) {
         console.log(err)
     }
 }
 
+const debounce = (fn, delay = 1500) => {
+    let timeoutId;
+    return (...args) => {
+        if (timeoutId) {
+            clearTimeout(timeoutId)
+        }
+        timeoutId = setTimeout(() => {
+            fn.apply(null, args);
+        }, delay)
+    }
+}
+
+const companySearch = async (event) => {
+    results = await fetchData(event.target.value);
+    console.log(results);
+    for (let i = 0; i < results.results.length; i++) {
+        companyResult = document.createElement('div');
+        companyResult.innerHTML = `<h1> ${results.results[i].organization_fields.lingo_account_code} ${results.results[i].name} `
+
+        searchDropdown.appendChild(companyResult);
+    }
+
+
+}
+
+
+const search = document.querySelector('input');
+search.addEventListener('input', debounce(companySearch));
+const searchDropdown = document.querySelector('#container');
