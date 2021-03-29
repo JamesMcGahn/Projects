b5f5307a3386b1aa296e108025d33dbef2a5d9cc
almag3st <jmcgahn@gmail.com>
2021-03-29 04:09:04 -0400

    added on new search remove previous search table, color styling & touch ups

--- Projects From Scratch/ZendeskCustomSearch/app.js
+++ Projects From Scratch/ZendeskCustomSearch/app.js
@@ -56,41 +56,59 @@ function renderDropdown(results) {
         companyResult = document.createElement('div');
         companyResult.classList.add('dropDown-item');
         companyResult.innerHTML = `<h6> ${results.results[i].organization_fields.lingo_account_code} ${results.results[i].name}</h6> `
+        searchOptions.classList.remove('hidden');
         companyResult.addEventListener('click', () => {
             console.log(results.results[i].id)
             ticketSearch(results.results[i].id);
             searchOptions.classList.add('hidden');
+            while (searchOptions.hasChildNodes()) {
+                searchOptions.removeChild(searchOptions.firstChild);
+            }
         })
         searchOptions.appendChild(companyResult);
     }
 }
 
 function renderTable(results) {
-    const tableContainer = document.querySelector('#tableContainer');
-    const table = document.createElement('table');
-    tableContainer.appendChild(table);
-    const tableHeader = document.createElement('tr');
-    tableHeader.innerHTML = `
+    const clearTable = document.querySelector('#results-table');
+    clearTable === !null ? clearTable.remove() : clearTable;
+    tableContainer.innerHTML = ``;
+    if (results.count > 0) {
+        const table = document.createElement('table');
+        table.id = 'results-table';
+        tableContainer.appendChild(table);
+        const tableHeader = document.createElement('tr');
+        tableHeader.innerHTML = `
     <th>Ticket #</th>
+    <th>Account Code</th>
     <th>Ticket Type</th>
     <th>Ticket Summary</th>
     <th>Status</th>
     `
-    table.appendChild(tableHeader);
-    ticketData = results.results;
-    for (let i = 0; i < ticketData.length; i++) {
-        const tableRow = document.createElement('tr')
-        let fields = results.results[i].fields;
-        let ticketType = fields[fields.findIndex(x => x.id == 360039468853)].value;
-        let ticketSummary = fields[fields.findIndex(x => x.id == 360041558474)].value;
-
-        tableRow.innerHTML = `
+        table.appendChild(tableHeader);
+        ticketData = results.results;
+        for (let i = 0; i < ticketData.length; i++) {
+            const tableRow = document.createElement('tr')
+            let fields = results.results[i].fields;
+            let ticketType = fields[fields.findIndex(x => x.id == 360039468853)].value;
+            let ticketSummary = fields[fields.findIndex(x => x.id == 360041558474)].value;
+            let ticketAcctCode = fields[fields.findIndex(x => x.id == 360042338954)].value;
+            tableRow.innerHTML = `
         <td><a href="${keys.DOMAIN}/agent/tickets/${ticketData[i].id}">${ticketData[i].id}</a></td>
+        <td>${ticketAcctCode.toUpperCase()}</td>
         <td>${ticketType}</td>
-        <td>${ticketSummary === null ? results.results[i].raw_subject : ticketSummary}</td>
+        <td id="td-summary">${ticketSummary === null ? results.results[i].raw_subject : ticketSummary}</td>
         <td><span class="${ticketData[i].status}"> ${ticketData[i].status}</span></td>
         `
-        table.appendChild(tableRow);
+            table.appendChild(tableRow);
+        }
+
+    } else {
+        tableContainer.innerHTML = `
+        <div class="no-results">
+        <h3> No Search Results Found </h3>
+        </div>
+        `;
     }
 
 }
@@ -100,3 +118,4 @@ const search = document.querySelector('input');
 search.addEventListener('input', debounce(companySearch));
 const searchDropdown = document.querySelector('#container');
 const searchOptions = document.querySelector('#searchOptions');
+const tableContainer = document.querySelector('#tableContainer');
