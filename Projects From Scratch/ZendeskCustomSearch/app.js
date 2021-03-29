c03f2d0ad8022906a6eca37eb9b07a560354b7f7
almag3st <jmcgahn@gmail.com>
2021-03-28 23:03:42 -0400

    helper funcs to support multiple ways of search, added ticket search func, event listeners for dropdown selection

--- Projects From Scratch/ZendeskCustomSearch/app.js
+++ Projects From Scratch/ZendeskCustomSearch/app.js
@@ -1,13 +1,11 @@
-const fetchData = async (searchTerm) => {
+const fetchData = async (params) => {
     try {
         const res = await axios.get(`${keys.DOMAIN}/api/v2/search.json`, {
             headers: {
-                Authorization: `${keys.API_KEY}`
+                Authorization: `${keys.API_KEY}`,
+                'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
             },
-            params: {
-                query: `lingo_account_code:${searchTerm}`,
-
-            }
+            params,
         })
         return res.data;
     } catch (err) {
@@ -28,16 +26,41 @@ const debounce = (fn, delay = 1500) => {
 }
 
 const companySearch = async (event) => {
-    results = await fetchData(event.target.value);
+    const regex = /(^[a-zA-Z]{3}\d{2}$)|(^[a-zA-Z]{5}$)/;
+    let results;
+    if (regex.test(event.target.value)) {
+        const params = new URLSearchParams();
+        params.append('query', `lingo_account_code:${event.target.value} type:organization`);
+        results = await fetchData(params);
+        renderDropdown(results)
+    } else if (event.target.value) {
+        const params = new URLSearchParams();
+        params.append('query', `type:organization "${event.target.value}"`);
+        results = await fetchData(params);
+        renderDropdown(results)
+    }
+}
+
+
+const ticketSearch = async (companyId) => {
+    const params = new URLSearchParams();
+    params.append('query', `organization:${companyId} type:ticket status<solved`);
+    results = await fetchData(params);
+    console.log(results)
+}
+
+function renderDropdown(results) {
     console.log(results);
     for (let i = 0; i < results.results.length; i++) {
         companyResult = document.createElement('div');
+        companyResult.classList.add('dropDown-item');
         companyResult.innerHTML = `<h1> ${results.results[i].organization_fields.lingo_account_code} ${results.results[i].name} `
-
+        companyResult.addEventListener('click', () => {
+            console.log(results.results[i].id)
+            ticketSearch(results.results[i].id);
+        })
         searchDropdown.appendChild(companyResult);
     }
-
-
 }
 
 
