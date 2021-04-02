bdfe03826b48713fa6cb6006094b29ecff3f2080
almag3st <jmcgahn@gmail.com>
2021-04-02 01:18:36 -0400

    filter todos/notes

--- Projects From Tuts/NoteTaker/index.js
+++ Projects From Tuts/NoteTaker/index.js
@@ -9,41 +9,76 @@ const notes = [{
     body: 'ham 2'
 }];
 
+const filters = {
+    searchText: ''
+}
+
+const renderNotes = function (notes, filters) {
+    const filteredNotes = notes.filter((notes) => {
+        return notes.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    document.querySelector('#notes').innerHTML = ``;
+    filteredNotes.forEach((note) => {
+        const noteEl = document.createElement('p');
+        noteEl.textContent = note.title;
+        document.querySelector('#notes').appendChild(noteEl);
+    })
+}
+renderNotes(notes, filters);
+
+document.querySelector('#create-note').addEventListener('click', (e) => {
+
+})
+document.querySelector('#remove-all').addEventListener('click', (e) => {
+
+})
+
+document.querySelector('#search-text').addEventListener('change', (e) => {
+    filters.searchText = e.target.value;
+    renderNotes(notes, filters);
+})
+
+
+
+
+
+
+
+
+
 // const findNote = function (notes, noteTitle) {
 //     const index = notes.findIndex((note, index) => {
 //         return note.title.tolowerCase() === noteTitle.tolowerCase()
 //     })
 //     return notes[index]
 // }
-const findNotes = function (notes, query) {
-    return filterNotes = notes.filter((note, index) => {
-        const isTitleMatch = note.title.tolowerCase().includes(query)
-        const isBodyMatch = note.title.tolowerCase().includes(query)
-        return isTitleMatch || isBodyMatch
-    })
-}
+// const findNotes = function (notes, query) {
+//     return filterNotes = notes.filter((note, index) => {
+//         const isTitleMatch = note.title.tolowerCase().includes(query)
+//         const isBodyMatch = note.title.tolowerCase().includes(query)
+//         return isTitleMatch || isBodyMatch
+//     })
+// }
 
-const sortNotes = function (notes) {
-    notes.sort((a, b) => {
-        if (a.title.tolowerCase() < b.title.tolowerCase()) {
-            return -1
-        } else if (b.title.tolowerCase() < a.title.tolowerCase()) {
-            return 1
-        } else {
-            return 0
-        }
-    })
-}
+// const sortNotes = function (notes) {
+//     notes.sort((a, b) => {
+//         if (a.title.tolowerCase() < b.title.tolowerCase()) {
+//             return -1
+//         } else if (b.title.tolowerCase() < a.title.tolowerCase()) {
+//             return 1
+//         } else {
+//             return 0
+//         }
+//     })
+// }
 
-const findNote2 = function (notes, noteTitle) {
-    return note = notes.find((note, index) => {
-        return note.title.tolowerCase() === noteTitle.tolowerCase()
-    })
-}
-
-
+// const findNote2 = function (notes, noteTitle) {
+//     return note = notes.find((note, index) => {
+//         return note.title.tolowerCase() === noteTitle.tolowerCase()
+//     })
+// }
 
 
 
-console.log(notes.length)
-console.log(notes)
+
--- Projects From Tuts/TodoApp/index.js
+++ Projects From Tuts/TodoApp/index.js
@@ -5,40 +5,83 @@ const todos = [
     { text: 'todo 4', completed: false }
 ];
 
-function removeTodo(todos, text) {
-    index = todo.findByIndex((x) => {
-        todos.text.toLowerCase() === text.toLowerCase();
+// function removeTodo(todos, text) {
+//     index = todo.findByIndex((x) => {
+//         todos.text.toLowerCase() === text.toLowerCase();
+//     })
+
+//     if (index > -1) {
+//         todos.splice(index, 1)
+//     }
+// }
+
+// const findtodos = function (todos, query) {
+//     return todos.filter((todo) => {
+//         return todo.completed === false
+//     })
+// }
+
+
+// const sortNotes = function (todos) {
+//     todos.sort((a, b) => {
+//         if (!a.completed && b.completed) {
+//             return -1
+//         } else if (!b.completed && a.completed) {
+//             return 1
+//         } else {
+//             return 0
+//         }
+//     })
+// }
+
+// todos.forEach(todo => {
+//     let newTodo = document.createElement('p');
+//     newTodo.innerText = todo.text;
+//     todolist.appendChild(newTodo)
+// })
+
+filter = {
+    searchText: ''
+}
+todolist = document.querySelector('#todo-list');
+
+function renderTodos(todos, filter) {
+    const filteredTodos = todos.filter(todos => {
+        return todos.text.toLowerCase().includes(filter.searchText.toLowerCase());
     })
 
-    if (index > -1) {
-        todos.splice(index, 1)
-    }
-}
-
-const findtodos = function (todos, query) {
-    return todos.filter((todo) => {
-        return todo.completed === false
+    todolist.innerHTML = ``;
+    const incompleteTodos = todos.filter(todo => {
+        return !todo.completed
     })
-}
 
 
-const sortNotes = function (todos) {
-    todos.sort((a, b) => {
-        if (!a.completed && b.completed) {
-            return -1
-        } else if (!b.completed && a.completed) {
-            return 1
-        } else {
-            return 0
-        }
+    let tasksLeft = document.createElement('p');
+    tasksLeft.innerText = `You have ${incompleteTodos.length} left`;
+    todolist.appendChild(tasksLeft);
+
+
+    filteredTodos.forEach(todo => {
+        newTodo = document.createElement('p');
+        newTodo.innerText = todo.text;
+        todolist.appendChild(newTodo);
     })
+
 }
 
-p = document.querySelectorAll('p');
 
-p.forEach(x => {
-    if (x.textContent.includes('the')) {
-        x.remove();
-    }
-});
+renderTodos(todos, filter)
 
+
+
+
+
+
+document.querySelector('#create-todo').addEventListener('click', (e) => {
+
+})
+
+document.querySelector('#filter-todo').addEventListener('input', (e) => {
+    filter.searchText = e.target.value;
+    renderTodos(todos, filter)
+})
