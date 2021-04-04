361c59507edc2601b73550079edc5f5d105ed00a
almag3st <jmcgahn@gmail.com>
2021-04-04 14:02:09 -0400

    Hangman start

---
+++ Projects From Tuts/HangmanGame/app.js
@@ -0,0 +1,21 @@
+const Hangman = function (word, remainingGuesses) {
+    this.word = word.toLowerCase().split('')
+    this.remainingGuesses = remainingGuesses
+    this.guessedLetters = []
+}
+
+Hangman.prototype.getPuzzle = function () {
+    let puzzle = ''
+
+    this.word.forEach(letter => {
+        if (this.guessedLetters.includes(letter) || letter === ' ') {
+            puzzle += letter
+        } else {
+            puzzle += '*'
+        }
+    });
+
+    return puzzle
+}
+
+const game1 = new Hangman(cat, 2)
