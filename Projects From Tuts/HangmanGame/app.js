8ef1ae67dd8c921a1cee647e818ae7531780bd15
almag3st <jmcgahn@gmail.com>
2021-04-05 20:09:45 -0400

    update the status msg on screen, stop user from guessing after all guesses are used up

--- Projects From Tuts/HangmanGame/app.js
+++ Projects From Tuts/HangmanGame/app.js
@@ -3,11 +3,11 @@ const guessesEl = document.querySelector('#guesses')
 const game1 = new Hangman('cat', 2)
 
 puzzleEl.textContent = game1.getPuzzle()
-guessesEl.textContent = game1.remainingGuesses
+guessesEl.textContent = game1.getStatusMessage()
 
 window.addEventListener('keypress', function (e) {
     const guess = String.fromCharCode(e.charCode);
     game1.makeGuess(guess);
     puzzleEl.textContent = game1.getPuzzle()
-    guessesEl.textContent = game1.remainingGuesses
+    guessesEl.textContent = game1.getStatusMessage()
 })
--- Projects From Tuts/HangmanGame/hangman.js
+++ Projects From Tuts/HangmanGame/hangman.js
@@ -19,6 +19,16 @@ Hangman.prototype.calculateStatus = function () {
     }
 }
 
+Hangman.prototype.getStatusMessage = function () {
+    if (this.status === 'playing') {
+        return `Guesses left ${this.remainingGuesses}`
+    } else if (this.status === 'failed') {
+        return `Nice Try! The word was "${this.word.join('')}"`
+    } else {
+        return `Nice. You got it!`
+    }
+}
+
 Hangman.prototype.getPuzzle = function () {
     let puzzle = ''
 
@@ -37,6 +47,11 @@ Hangman.prototype.makeGuess = function (guess) {
     guess = guess.toLowerCase()
     const isUnique = !this.guessedLetters.includes(guess)
     const isBadGuess = !this.word.includes(guess)
+
+    if (this.status !== 'playing') {
+        return
+    }
+
     if (isUnique) {
         this.guessedLetters.push(guess)
     }
