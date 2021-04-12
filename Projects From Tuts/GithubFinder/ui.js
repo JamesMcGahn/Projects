3e89cc41774dde31d5b21c4491faf0ac1a047a24
almag3st <jmcgahn@gmail.com>
2021-04-11 23:31:56 -0400

    display repos

--- Projects From Tuts/GithubFinder/ui.js
+++ Projects From Tuts/GithubFinder/ui.js
@@ -18,9 +18,9 @@ class UI {
             <span class="badge badge-info">Public Following: ${user.following}</span>
        <br><br>
        <ul class="list-group"> 
-            <li class="list-group-item">Company: ${user.company}</li>   
-            <li class="list-group-item">Website: ${user.blog}</li>   
-            <li class="list-group-item">Location: ${user.location}</li>   
+            <li class="list-group-item">Company: ${user.company === null ? 'Not Listed' : user.company}</li>   
+            <li class="list-group-item">Website: ${user.blog === '' ? 'Not Listed' : user.blog}</li>   
+            <li class="list-group-item">Location: ${user.location === null ? 'Not Listed' : user.location}</li>   
             <li class="list-group-item">Member Since: ${user.created_at}</li>   
        </ul>
             </div>
@@ -30,6 +30,28 @@ class UI {
         <div id="repos"></div>
         `
     }
+    showRepos(repos) {
+        let output = '';
+        repos.forEach(repo => {
+            output += `
+            <div class="card card-body mb-2">
+               <div class="row">
+            <div class="col-md-6">
+                <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+            </div>
+            <div class="col-md-6">
+            <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+            <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+            <span class="badge badge-success">Forks: ${repo.forks_count}</span>
+            </div>
+             </div>
+            </div>
+            `
+        });
+        document.querySelector('#repos').innerHTML = output;
+    }
+
+
     clearProfile() {
         this.profile.innerHTML = ``
     }
