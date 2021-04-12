4889d4857b63ad63d20defb8947ade032156303a
almag3st <jmcgahn@gmail.com>
2021-04-12 02:20:09 -0400

    display data in ui

--- Projects From Tuts/WeatherApp/app.js
+++ Projects From Tuts/WeatherApp/app.js
@@ -1,9 +1,10 @@
 const weather = new Weather('Boston', 'MA')
-
+const ui = new UI
 function getWeather() {
     weather.getWeather()
         .then(results => {
             console.log(results)
+            ui.paint(results)
         })
         .catch(err => console.log(err))
 }
