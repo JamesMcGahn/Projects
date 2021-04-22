edf3259789941fd245011596b3b4efd2a1a02284
almag3st <jmcgahn@gmail.com>
2021-04-22 18:24:32 -0400

    nav bar link smooth scroll, tabbed content display

--- Projects From Tuts/BankistSite/script.js
+++ Projects From Tuts/BankistSite/script.js
@@ -4,7 +4,10 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
+// modal open /close 
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -29,9 +32,8 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
 
+// hero button scroll
 btnScrollTo.addEventListener('click', function (e) {
 
   const s1coords = section1.getBoundingClientRect();
@@ -43,4 +45,31 @@ btnScrollTo.addEventListener('click', function (e) {
   // })
   //modern browsers only
   section1.scrollIntoView({ behavior: 'smooth' })
+})
+
+document.querySelector('.nav__links').addEventListener('click', (e) => {
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href')
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' })
+  }
+})
+
+// tabbed content
+const tabs = document.querySelectorAll('.operations__tab')
+const tabsContainer = document.querySelector('.operations__tab-container')
+const tabsContent = document.querySelectorAll('.operations__content')
+
+tabsContainer.addEventListener('click', (e) => {
+  const clicked = e.target.closest('.operations__tab')
+  if (!clicked) return
+  tabs.forEach(tab => {
+    tab.classList.remove('operations__tab--active')
+  })
+  tabsContent.forEach(tab => {
+    tab.classList.remove('operations__content--active')
+  })
+  clicked.classList.add('operations__tab--active')
+
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active')
 })
