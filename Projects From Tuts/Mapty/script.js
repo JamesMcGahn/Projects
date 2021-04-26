3d71f7bcd643a282118314992aada2302be85b97
James McGahn <jmcgahn@gmail.com>
2021-04-25 20:16:01 -0400

    on cmap click and form submit - display map marker, form change event for type

--- Projects From Tuts/Mapty/script.js
+++ Projects From Tuts/Mapty/script.js
@@ -10,12 +10,14 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+let map, mapEvent;
+
 
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
         const { latitude, longitude } = position.coords;
 
-        const map = L.map('map').setView([latitude, longitude], 13);
+        map = L.map('map').setView([latitude, longitude], 13);
 
         L.tileLayer(`https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png`, {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -25,21 +27,36 @@ if (navigator.geolocation) {
             .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
             .openPopup();
 
-        map.on('click', function (mapEvent) {
-            const { lat, lng } = mapEvent.latlng
+        map.on('click', function (mapE) {
+            mapEvent = mapE;
+            form.classList.remove('hidden')
+            inputDistance.focus();
 
-            L.marker([lat, lng]).addTo(map)
-                .bindPopup(L.popup({
-                    maxWidth: 250,
-                    minWidth: 100,
-                    autoClose: false,
-                    closeOnClick: false,
-                    className: 'running-popup'
-                }))
-                .setPopupContent('Hi')
-                .openPopup();
         })
 
     }, function () { })
 }
 
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+
+
+    const { lat, lng } = mapEvent.latlng
+
+    L.marker([lat, lng]).addTo(map)
+        .bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup'
+        }))
+        .setPopupContent('Hi')
+        .openPopup();
+})
+
+inputType.addEventListener('change', function () {
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
+})
