0ef6a4bde5ca44df97a6fc12ad5a62abfffdd47e
James McGahn <jmcgahn@gmail.com>
2021-04-28 23:15:32 -0400

    implemented search call

--- Projects From Tuts/Forkify/src/js/controller.js
+++ Projects From Tuts/Forkify/src/js/controller.js
@@ -1,5 +1,6 @@
 import * as model from './model.js'
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView';
 
 
 import 'core-js/stable'
@@ -30,7 +31,20 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    const query = searchView.getQuery();
+    if (!query) return
+
+    await model.loadSearchResults(query)
+    console.log(model.state.search.results)
+  } catch (err) {
+    console.log(err)
+  }
+}
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes)
+  searchView.addHandlerSearch(controlSearchResults)
 }
 init();
