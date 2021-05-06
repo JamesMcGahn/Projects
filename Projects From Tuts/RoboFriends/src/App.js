8b16d869597ecf3aa3f2a0a72eb2dac96cef66a6
James McGahn <jmcgahn@gmail.com>
2021-05-05 20:16:47 -0400

    added fetching users from api

--- Projects From Tuts/RoboFriends/src/App.js
+++ Projects From Tuts/RoboFriends/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import CardList from './cardList';
-import { robots } from './robots';
 import SearchBox from './SearchBox';
 import './App.css'
 
@@ -8,10 +7,19 @@ class App extends React.Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(users => this.setState({ robots: users }))
+    }
+
+
+
     onSearchChange = (event) => {
         this.setState({ searchfield: event.target.value })
 
@@ -21,11 +29,15 @@ class App extends React.Component {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        return (<div>
-            <h1 className="f2">RoboFriends</h1>
-            <SearchBox searchChange={this.onSearchChange} />
-            <CardList robots={filteredRobots} />
-        </div >)
+        if (this.state.robots.length === 0) {
+            return <h1>No robots found</h1>
+        } else {
+            return (<div>
+                <h1 className="f2">RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={filteredRobots} />
+            </div >)
+        }
     }
 }
 
