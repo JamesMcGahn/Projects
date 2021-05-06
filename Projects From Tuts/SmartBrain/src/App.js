61814a380e850a314b5a6278fbde80993851980f
James McGahn <jmcgahn@gmail.com>
2021-05-06 18:15:01 -0400

    basic nav and logo

--- Projects From Tuts/SmartBrain/src/App.js
+++ Projects From Tuts/SmartBrain/src/App.js
@@ -1,12 +1,13 @@
-
+import Navigation from './components/navigation/Navigation'
+import Logo from './components/logo/Logo'
 import './App.css';
-
+import 'tachyons';
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/* <Logo />
-      <ImageLinkForm />
+      <Logo />
+      {/* <ImageLinkForm />
       <FaceRecognition /> */}
     </div>
   );
