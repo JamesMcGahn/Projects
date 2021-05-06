50dc84479332ce0821249cde86f667f5127ab755
James McGahn <jmcgahn@gmail.com>
2021-05-05 21:33:33 -0400

    added error boundary, readme & gif

--- Projects From Tuts/RoboFriends/src/containers/App.js
+++ Projects From Tuts/RoboFriends/src/containers/App.js
@@ -2,46 +2,48 @@ import React from 'react';
 import CardList from '../components/cardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
-import './App.css'
+import ErrorBoundry from '../components/ErrorBoundry';
+import './App.css';
 
 class App extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: '',
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((users) => this.setState({ robots: users }));
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+
+  render() {
+    const filteredRobots = this.state.robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    });
+    if (this.state.robots.length === 0) {
+      return <h1>No robots found</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f2">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <ErrorBoundry>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundry>
+          </Scroll>
+        </div>
+      );
     }
-
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(res => res.json())
-            .then(users => this.setState({ robots: users }))
-    }
-
-
-
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value })
-
-    }
-
-    render() {
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-        })
-        if (this.state.robots.length === 0) {
-            return <h1>No robots found</h1>
-        } else {
-            return (<div className='tc'>
-                <h1 className="f2">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <Scroll>
-                    <CardList robots={filteredRobots} />
-                </Scroll>
-            </div >)
-        }
-    }
+  }
 }
 
 export default App;
