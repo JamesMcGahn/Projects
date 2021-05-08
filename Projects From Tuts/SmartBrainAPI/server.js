9a664fa9620791ebf1407190fbf7ae5985b10c3e
James McGahn <jmcgahn@gmail.com>
2021-05-07 23:14:02 -0400

    api skeleton done

---
+++ Projects From Tuts/SmartBrainAPI/server.js
@@ -0,0 +1,82 @@
+const express = require("express");
+
+
+
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+const database = {
+    users: [
+        {
+            id: 123,
+            name: 'John',
+            email: 'john@example.com',
+            password: 'cheese',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
+app.get('/', (req, res) => {
+    res.send('this works')
+})
+
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+        res.json('hi')
+    } else {
+        res.status(401).json('error logging in')
+    }
+})
+
+app.post('/register', (req, res) => {
+    database.users.push({
+        id: 1234,
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        entries: 0,
+        joined: new Date()
+    })
+    res.json(database.users[database.users.length - 1])
+})
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    console.log(id)
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === +id) {
+
+            found = true
+            return res.json(user)
+        }
+    })
+    if (!found) {
+        res.status(400).json('cant find')
+    }
+})
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === +id) {
+
+            found = true
+            user.entries++
+            return res.json(user.entries)
+        }
+    })
+    if (!found) {
+        res.status(400).json('cant find')
+    }
+})
+
+app.listen(3001, () => {
+    console.log('app is firing on 3001')
+})
