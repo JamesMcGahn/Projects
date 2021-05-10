eca3763288c9475e7aab87cf470f46c3f1776095
James McGahn <jmcgahn@gmail.com>
2021-05-09 20:48:07 -0400

    basic todo structure

--- Projects From Tuts/TodoApp2/index.js
+++ Projects From Tuts/TodoApp2/index.js
@@ -1,12 +1,32 @@
 const express = require('express');
+const path = require('path');
 
 app = express();
+
+let todos = []
+
+
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
-
+app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
-    res.send('hi')
+    const today = new Date();
+    const day = today.toLocaleDateString('en-US',
+        {
+            weekday: "long",
+            day: "numeric",
+            month: "long"
+        })
+
+    res.render("list", { today: day, todos: todos })
+})
+
+app.post('/', (req, res) => {
+    const newItem = req.body.newItem
+    todos.push(newItem)
+    res.redirect('/')
 })
 
 app.listen(3000, () => {
