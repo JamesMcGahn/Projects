efe32cf5db1b82724a3cf2134a3e97c6fcb632da
James McGahn <jmcgahn@gmail.com>
2021-06-05 01:28:26 -0400

    converted about me to dynamic template

--- Projects From Scratch/PolWPSite/wordpress/wp-content/themes/paul-sarlo/Page-aboutpaul.php
+++ Projects From Scratch/PolWPSite/wordpress/wp-content/themes/paul-sarlo/Page-aboutpaul.php
@@ -12,18 +12,13 @@ get_header();
                 <div id="biocontainer" class="col-md-8 col-sm-12 card">
 
                     <div class="row bioheader">
-                        <h1>Senator Paul Sarlo</h1>
+                        <h1><?php the_title(); ?></h1>
                     </div>
                     <div class="row biocontent">
                         <div class="row">
-                            <div class="col-md-3 col-sm-12 bioimage"><img src="<?php bloginfo( 'stylesheet_directory'); ?>/assets/img/pas_profile.jpeg"></div>
+                            <div class="col-md-3 col-sm-12 bioimage"><img src="<?php echo wp_get_attachment_url( get_post_thumbnail_id( $post-> ID)); ?>"></div>
                             <div class="col-md-9 col-sm-12 bio">
-                                <h4>Official Biography of Senator Paul A. Sarlo</h4>
-                                <h5><strong>Deputy Majority Leader, New Jersey Senate</strong></h5>
-                                    <h6><em>Democrat of District 36</em></h6>
-                                <p>Paul A. Sarlo is the Deputy Majority Leader of the New Jersey Senate.  He serves as chairman of the Budget and Appropriations Committee and is a member of the Judiciary Committee, the Higher Education Committee, the Joint Budget Oversight Committee, and the Senate Legislative Oversight Committee.  He is a former chairman of the Judiciary Committee and the Labor Committee.</p>
-                                <p>He has also sponsored bills which reformed New Jersey’s worker’s compensation system, criminalized the illegal trafficking and distribution of prescription drugs, required schools to adopt bullying prevention policies and upgraded penalties for identity theft.</p>
-                                <p>Mr. Sarlo’s career in public service spans more than two decades. He has served as Mayor of the Borough of Wood-Ridge since 2000 and served on its Borough Council from 1995-2000.  In 2001, he was elected to the New Jersey General Assembly, a position he held until May 2003 when he was sworn in to the New Jersey Senate to fill an unexpired term.  He was elected to a full term in the New Jersey Senate in November 2003 and was re-elected in 2007, 2011, and 2013.  Mr. Sarlo represents the 36th legislative district, which includes 15 municipalities in Bergen and Passaic Counties.</p>
+                            <?php the_content(); ?>
                             </div>
                                 
                             </div>
