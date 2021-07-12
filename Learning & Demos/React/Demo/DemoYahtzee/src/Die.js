cd1bdddbe8a5866520d7663bd1bb77cdd19b69c8
almag3st <jmcgahn@gmail.com>
2021-07-11 23:56:02 -0400

    yahtzee demo

--- Learning & Demos/React/Demo/DemoYahtzee/src/Die.js
+++ Learning & Demos/React/Demo/DemoYahtzee/src/Die.js
@@ -2,14 +2,26 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
+  static defaultProps = {
+    wordNums: ["one", "two", "three", "four", "five", "six"],
+    val: 1
+  }
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.props.handleClick(this.props.idx)
+  }
   render() {
+    let diceIcon = `Die fas fa-dice-${this.props.wordNums[this.props.val - 1]} fa-5x`
     return (
       <button
         className={"Die"}
         style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={this.props.handleClick}
+        onClick={this.handleClick}
       >
-        {this.props.val}
+        <i className={diceIcon}></i>
       </button>
     );
   }
