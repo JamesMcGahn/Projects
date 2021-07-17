a686200916b2cc66cbd667e5d182da868d9f6078
almag3st <jmcgahn@gmail.com>
2021-07-16 23:29:31 -0400

    added palette, colorbox component and basic starting styles

--- Projects From Tuts/React Projects/colorapp/src/App.js
+++ Projects From Tuts/React Projects/colorapp/src/App.js
@@ -1,6 +1,6 @@
-
 import React, { Component } from 'react'
-
+import Palette from './Palette'
+import seedColors from './SeedColors'
 import './App.css';
 
 
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div>
-
+        <Palette palette={seedColors[5]} />
       </div>
     )
   }
