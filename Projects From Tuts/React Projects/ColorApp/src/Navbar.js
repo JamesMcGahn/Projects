de8f085e8f733e1ad8509e7024807a1e298b2cb0
almag3st <jmcgahn@gmail.com>
2021-07-17 23:40:55 -0400

    Add material - Select, snackbar.  add footer. styles

--- Projects From Tuts/React Projects/ColorApp/src/Navbar.js
+++ Projects From Tuts/React Projects/ColorApp/src/Navbar.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react'
 import Slider from 'rc-slider'
+import Select from '@material-ui/core/Select'
+import MenuItem from '@material-ui/core/MenuItem'
+import Snackbar from '@material-ui/core/Snackbar'
+import CloseIcon from '@material-ui/icons/Close'
+import IconButton from '@material-ui/core/IconButton'
 import 'rc-slider/assets/index.css'
 import './Navbar.css'
 
 class Navbar extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            format: "hex",
+            open: false
+        }
+
+        this.handleFormatChange = this.handleFormatChange.bind(this)
+        this.closeSnackBar = this.closeSnackBar.bind(this)
+    }
+
+    handleFormatChange(e) {
+        this.setState({ format: e.target.value, open: true })
+        this.props.changeFormat(e.target.value)
+    }
+
+    closeSnackBar() {
+        this.setState({ open: false })
+    }
+
     render() {
         return (
             <header>
@@ -19,8 +44,27 @@ class Navbar extends Component {
                                 min={100} max={900} step={100} onAfterChange={this.props.changeLevel} />
                         </div>
                     </div>
+                    <div className="select-container">
+                        <Select value={this.state.format} onChange={this.handleFormatChange}>
+                            <MenuItem value='hex'>HEX</MenuItem>
+                            <MenuItem value='rgb'>RGB</MenuItem>
+                            <MenuItem value='rgba'>RGBA</MenuItem>
+                        </Select>
+                    </div>
                 </div>
-            </header>
+                <Snackbar anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+                    open={this.state.open}
+                    autoHideDuration={3000}
+                    message={<span id='message-id'>Format Changed to {this.state.format.toUpperCase()}</span>}
+                    ContentProps={{
+                        'aria-describedby': 'message-id'
+                    }}
+                    onClose={this.closeSnackBar}
+                    action={[<IconButton color='inherit' key='close' aria-label='close' >
+                        <CloseIcon onClick={this.closeSnackBar} />
+                    </IconButton>]}
+                />
+            </header >
         )
     }
 }
--- Projects From Tuts/React Projects/ColorApp/src/Palette.js
+++ Projects From Tuts/React Projects/ColorApp/src/Palette.js
@@ -10,25 +10,35 @@ class Palette extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            level: 500
+            level: 500,
+            format: 'hex'
         }
         this.changeLevel = this.changeLevel.bind(this)
+        this.changeFormat = this.changeFormat.bind(this)
     }
 
     changeLevel(level) {
         this.setState({ level: level })
     }
+
+    changeFormat(format) {
+        this.setState({ format: format })
+    }
+
     render() {
         const colorBoxes = this.props.palette.colors[this.state.level].map(color =>
-            <ColorBox background={color.hex} name={color.name} />)
+            <ColorBox background={color[this.state.format]} name={color.name} key={color.id} />)
         return (
             <div className="Palette">
-                <Navbar level={this.state.level} changeLevel={this.changeLevel} />
+                <Navbar level={this.state.level} changeFormat={this.changeFormat} changeLevel={this.changeLevel} />
 
                 <div className="Palette-colors">
                     {colorBoxes}
-
                 </div>
+                <footer className="Palette-footer">
+                    {this.props.palette.paletteName}
+                    <span className='emoji'>{this.props.palette.emoji}</span>
+                </footer>
             </div>
         )
     }
