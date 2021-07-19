82b023fcf08cc74e9b1945d2e54deae4bdb8104e
almag3st <jmcgahn@gmail.com>
2021-07-18 20:19:28 -0400

    add react router + routes, add PaletteList, MiniPalette  component, add JSS styles for PaletteList, MiniPalette

--- Projects From Tuts/React Projects/colorapp/src/App.js
+++ Projects From Tuts/React Projects/colorapp/src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
 import Palette from './Palette'
+import PaletteList from './PaletteList'
 import seedColors from './SeedColors'
 import './App.css';
 import { generatePalette } from './colorHelpers'
 
 class App extends Component {
+  findPalette(id) {
+    return seedColors.find((palette) => palette.id === id)
+  }
   render() {
     return (
-      <div>
-        <Palette palette={generatePalette(seedColors[5])} />
-      </div>
+      <Switch>
+        <Route exact path='/' render={() => <PaletteList palettes={seedColors} />} />
+        <Route exact path='/palette/:id' render={(routeProps) => <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />} />
+      </Switch>
     )
   }
 }
