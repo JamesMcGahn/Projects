bc74e3fd2b7c9ad872a592531983c4ede4a42583
James McGahn <jmcgahn@gmail.com>
2021-08-01 23:00:16 -0400

    add context &  ho,

--- Learning & Demos/React/Demo/DemoContext/src/Navbar.js
+++ Learning & Demos/React/Demo/DemoContext/src/Navbar.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import styles from "./styles/NavbarStyles";
 
+import { ThemeContext } from "./contexts/ThemeContext";
+
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
@@ -9,30 +11,49 @@ import InputBase from "@material-ui/core/InputBase";
 import SearchIcon from "@material-ui/icons/Search";
 import Switch from "@material-ui/core/Switch";
 import { withStyles } from "@material-ui/core/styles";
+import { withLanguageContext } from "./contexts/LanguageContext";
 
+const content = {
+    english: {
+        search: "Search",
+        flag: "🇬🇧"
+    },
+    french: {
+        search: "Chercher",
+        flag: "🇫🇷"
+    },
+    spanish: {
+        search: "Buscar",
+        flag: "🇪🇸"
+    }
+};
 
 
 class Navbar extends Component {
+    static contextType = ThemeContext
     render() {
         const { classes } = this.props;
+        const { isDarkMode, toggleDarkMode } = this.context
+        const { language } = this.props.languageContext
+        const { search, flag } = content[language]
         return (
             <div className={classes.root}>
-                <AppBar position='static' color='primary'>
+                <AppBar position='static' color={isDarkMode ? "black" : "white"}>
                     <Toolbar>
                         <IconButton className={classes.menuButton} color='inherit'>
-                            <span>🇫🇷</span>
+                            <span>{flag}</span>
                         </IconButton>
                         <Typography className={classes.title} variant='h6' color='inherit'>
                             App Title
                         </Typography>
-                        <Switch />
+                        <Switch onChange={toggleDarkMode} />
                         <div className={classes.grow} />
                         <div className={classes.search}>
                             <div className={classes.searchIcon}>
                                 <SearchIcon />
                             </div>
                             <InputBase
-                                placeholder='Search...'
+                                placeholder={search}
                                 classes={{
                                     root: classes.inputRoot,
                                     input: classes.inputInput
@@ -45,4 +66,4 @@ class Navbar extends Component {
         );
     }
 }
-export default withStyles(styles)(Navbar);
+export default withLanguageContext(withStyles(styles)(Navbar));
