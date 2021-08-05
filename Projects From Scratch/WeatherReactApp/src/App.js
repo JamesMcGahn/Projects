c8e1c08c3d0d6e27457571edf7181bb8575a0511
James McGahn <jmcgahn@gmail.com>
2021-08-05 19:13:15 -0400

    nav, forecast card, form comps added, fetch data added

--- Projects From Scratch/WeatherReactApp/src/App.js
+++ Projects From Scratch/WeatherReactApp/src/App.js
@@ -1,23 +1,127 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react';
+import Forecast from './Forecast';
+import Form from './Form';
+import Navbar from './Navbar';
 
-function App() {
+import axios from 'axios'
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+
+const drawerWidth = 240;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+}));
+
+
+
+
+function App(props) {
+
+  const [open, setOpen] = React.useState('false');
+  const [city, setCity] = React.useState('Miami');
+  const [stateLoc, setStateLoc] = React.useState('FL');
+  const [country, Setcountry] = React.useState('US');
+  const [weatherData, setweatherData] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const weatherFetch = async () => {
+    try {
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${stateLoc},${country}&units=imperial&appid=`);
+      const { data } = response
+      console.log(data)
+      return setweatherData(data)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+
+  useEffect(() => {
+    weatherFetch()
+  }, [city])
+
+
+  const classes = useStyles();
+  const theme = useTheme();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <CssBaseline />
+      <Navbar handleDrawerOpen={handleDrawerOpen} open={open} />
+      <Form open={open} handleDrawerOpen={handleDrawerOpen} handleDrawerClose={handleDrawerClose} />
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        <div className={classes.drawerHeader} />
+        {weatherData &&
+          <Forecast weather={weatherData} handleDrawerOpen={handleDrawerOpen} />
+        }
+      </main>
     </div>
   );
 }
---
+++ Projects From Scratch/WeatherReactApp/src/Forecast.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+
+const useStyles = makeStyles({
+    root: {
+        minWidth: 275,
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 12,
+    },
+});
+
+
+function Forecast({ weather, handleDrawerOpen }) {
+    const classes = useStyles()
+    return (
+        <Card className={classes.root}>
+            <CardContent>
+                <Typography gutterBottom variant="h3" component="h1">
+                    {`${weather.name}`}
+                </Typography>
+
+                <List>
+                    <ListItem>
+                        <ListItemText primary={`${weather.weather[0].description.replace(/\b\w/g, l => l.toUpperCase())}`} />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary={`Relative Humidity: ${weather.main.humidity}`} />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary={`Feels Like: ${weather.main.feels_like}`} />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary={`Min: ${weather.main.temp_min}° F, Max: ${weather.main.temp_max}° F`} />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary={`Wind Gusts: ${weather.wind.gust} mph`} />
+                    </ListItem>
+                </List>
+
+
+
+                {`${weather.main.temp}° F`}
+                {`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+
+
+            </CardContent>
+            <CardActions>
+                <Button size="large" onClick={handleDrawerOpen}>Change Location</Button>
+            </CardActions>
+        </Card>
+    );
+}
+
+export default Forecast;
