da3be4580775801a67fbc7e48755df131f5974e1
James McGahn <jmcgahn@gmail.com>
2021-08-12 20:03:58 -0400

    layout, header, homepage, search comps, modified button to handle buttons without link

--- Learning & Demos/Next.js/Demo/DemoEvents/components/ui/button.js
+++ Learning & Demos/Next.js/Demo/DemoEvents/components/ui/button.js
@@ -2,10 +2,18 @@ import React from 'react';
 import Link from 'next/link'
 import classes from './button.module.css';
 
-function button({ id, children }) {
-    return (
-        <Link href={'/' + id}><a className={classes.btn}>{children}</a></Link>
-    );
+function button({ id, children, onClick }) {
+    if (id) {
+        return (
+            <Link href={'/' + id}><a className={classes.btn}>{children}</a></Link>
+        );
+    }
+    else {
+        return (
+            <button className={classes.btn} onClick={onClick}>{props.children}</button>
+        );
+    }
+
 }
 
 export default button;
--- Learning & Demos/Next.js/Demo/DemoEvents/pages/events/index.js
+++ Learning & Demos/Next.js/Demo/DemoEvents/pages/events/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import { getAllEvents } from '../../dummy-data'
+import EventList from '../../components/events/eventList'
+import EventsSearch from '../../components/events/eventsSearch'
 function AllEventsPage(props) {
+    const events = getAllEvents()
+
     return (
         <div>
-            <h1>Events</h1>
+            <EventsSearch />
+            <EventList events={events} />
         </div>
     );
 }
