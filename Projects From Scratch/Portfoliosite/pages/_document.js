384d2989615bf67854ec7ee090440de194f63198
James McGahn <jmcgahn@gmail.com>
2021-09-06 13:20:45 -0400

    styling and meta adjustments

--- Projects From Scratch/Portfoliosite/pages/_document.js
+++ Projects From Scratch/Portfoliosite/pages/_document.js
@@ -14,9 +14,15 @@ class MyDocument extends Document {
                     <meta name="keywords" itemProp="keywords" content="james mcgahn, developer, fontend developer, mcgahn"></meta>
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+                    <meta property="og:image" />
                     <meta name="google-site-verification" content="l5RoOAbWHSGKKacvY4FJGlFQtCuFLbbSiJ8HRLjTG8A" />
                     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet" />
                     <link rel="shortcut icon" href="/favicon.ico" />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:url" content="https://www.jamesmcgahn.com/" />
+                    <meta property="og:title" content="James McGahn" />
+                    <meta property="og:description" content="James McGahn" />
+                    <meta property="og:image" content="https://www.jamesmcgahn.com/img/jmlogo3.jpg" />
                 </Head>
                 <body>
                     <Main />
