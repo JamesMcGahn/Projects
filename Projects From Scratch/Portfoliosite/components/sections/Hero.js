583ff827cdcdd8753006a632e766ca7429a58292
James McGahn <jmcgahn@gmail.com>
2021-10-05 20:23:38 -0400

    switched  hero background image - to next image to help with load time

--- Projects From Scratch/Portfoliosite/components/sections/Hero.js
+++ Projects From Scratch/Portfoliosite/components/sections/Hero.js
@@ -3,13 +3,19 @@ import Container from 'react-bootstrap/Container';
 import classes from '../../styles/hero.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
-
+import Image from 'next/image'
 function Hero({ h1, h2 }) {
     return (
         <React.Fragment>
             <Container className={classes.hero} fluid>
                 <h1 id={classes.h1}>{h1}</h1>
                 <h2 id={classes.h2}>{h2}</h2>
+                <Image
+                    src="/img/jmlogo3.jpg"
+                    alt="hero"
+                    layout="fill"
+                    objectFit="cover"
+                />
                 <div className={classes.chevron}><a href="#about"><FontAwesomeIcon icon={faChevronDown} /></a></div>
             </Container>
         </React.Fragment >
