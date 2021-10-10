2544f2164e6bcb9d393bf351426c26fb6cdeaa7e
James McGahn <jmcgahn@gmail.com>
2021-10-10 17:05:24 -0400

    added loading state for hero to fix bad ui experience of image loading on 4k

--- Projects From Scratch/Portfoliosite/components/sections/Hero.js
+++ Projects From Scratch/Portfoliosite/components/sections/Hero.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import classes from '../../styles/hero.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
 import Image from 'next/image'
+
+
+
+
 function Hero({ h1, h2 }) {
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        setLoading(true)
+    }, [])
+
+
     return (
         <div className={classes.heroCont}>
-            <Container className={classes.hero} fluid>
+            <Container className={loading ? classes.hero : classes.heroLoading} fluid>
                 <h1 id={classes.h1}>{h1}</h1>
                 <h2 id={classes.h2}>{h2}</h2>
                 <Image
