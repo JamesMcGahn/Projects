9eefc984658bb1de2c7bbbb799de2135e21546f4
James McGahn <jmcgahn@gmail.com>
2021-11-09 21:20:02 -0500

    refactor basic test  crud routes

--- Projects From Tuts/Node.js/NatourAPI/app.js
+++ Projects From Tuts/Node.js/NatourAPI/app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
     res.status(200).json({
         success: true,
         results: tours.length,
@@ -15,23 +15,53 @@ app.get('/api/v1/tours', (req, res) => {
             tours: tours
         }
     })
-})
+}
 
-app.post('/api/v1/tours', (req, res) => {
+const createTours = (req, res) => {
     const newId = tours[tours.length - 1].id + 1;
     const newTour = Object.assign({ id: newId }, req.body)
     tours.push(newTour);
     fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
         res.status(201).json({
-            success: false,
             status: 'success',
             data: {
                 tour: newTour
             }
         })
     })
-})
+}
 
+const getTour = (req, res) => {
+    const id = req.params.id * 1
+    const tour = tours.find(el => el.id === id)
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: tour
+        }
+    })
+}
+
+const updateTour = (req, res) => {
+    res.status(200).json({
+        status: 'success'
+    })
+}
+
+const deleteTour = (req, res) => {
+    res.status(200).json({
+        status: 'success'
+    })
+}
+
+app.route('/api/v1/tours')
+    .get(getAllTours)
+    .post(createTours)
+
+app.route('/api/v1/tours/:id')
+    .get(getTour)
+    .patch(updateTour)
+    .delete(deleteTour)
 
 const port = 3000;
 app.listen(port, () => {
