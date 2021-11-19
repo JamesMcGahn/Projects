fee744ecc84a006e3f8bc86207fbda37ae0e6cdb
James McGahn <jmcgahn@gmail.com>
2021-11-19 08:31:03 -0500

    update post handler to use tour model

--- Projects From Tuts/Node.js/NatourAPI/controllers/tourController.js
+++ Projects From Tuts/Node.js/NatourAPI/controllers/tourController.js
@@ -1,26 +1,30 @@
 const Tour = require('../models/tourModel');
 
-exports.checkBody = (req, res, next) => {
-    if (!req.body || !req.body.name || !req.body.price) {
-        return res.status(400).json({
-            status: 'fail',
-            message: 'Missing name or price',
-        });
-    }
-    next();
-};
-
 exports.getAllTours = (req, res) => {
     res.status(200).json({
         success: true,
     });
 };
 
-exports.createTour = (req, res) => {
-    res.status(201).json({
-        status: 'success',
-        data: {},
-    });
+exports.createTour = async (req, res) => {
+    try {
+        const newTour = await Tour.create(req.body);
+
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour,
+            },
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'failure',
+            data: {
+                message: 'There was a problem creating the tour',
+            },
+        });
+    }
+
 };
 
 exports.getTour = (req, res) => {
--- Projects From Tuts/Node.js/NatourAPI/routes/tourRoutes.js
+++ Projects From Tuts/Node.js/NatourAPI/routes/tourRoutes.js
@@ -3,11 +3,9 @@ const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
-router.param('id', tourController.checkID);
-
 router.route('/')
     .get(tourController.getAllTours)
-    .post(tourController.checkBody, tourController.createTours);
+    .post(tourController.createTour);
 
 router.route('/:id')
     .get(tourController.getTour)
