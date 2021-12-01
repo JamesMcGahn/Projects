bee0880196b01313da71f5045fed127e91f99301
James McGahn <jmcgahn@gmail.com>
2021-12-01 01:06:34 -0500

    month plan  aggregation

--- Projects From Tuts/Node.js/NatourAPI/controllers/tourController.js
+++ Projects From Tuts/Node.js/NatourAPI/controllers/tourController.js
@@ -143,3 +143,57 @@ exports.getTourStats = async (req, res) => {
         });
     }
 };
+
+exports.getMonthlyPlan = async (req, res) => {
+    try {
+        const year = req.params.year * 1;
+
+        const plan = await Tour.aggregate([
+            {
+                $unwind: '$startDates',
+            },
+            {
+                $match: { startDates: { $gte: new Date(`${year}-01-01`), $lte: new Date(`${year}-12-31`) } },
+            },
+            {
+                $group: {
+                    _id: { $month: '$startDates' },
+                    numOfToursStarts: { $sum: 1 },
+                    tours: { $push: '$name' },
+                    numRating: { $sum: '$ratingQuantity' },
+                    avgRating: { $avg: '$ratingAverage' },
+                    avgPrice: { $avg: '$price' },
+                    minPrice: { $min: '$price' },
+                    maxPrice: { $max: '$price' },
+                },
+            },
+            {
+                $addFields: {
+                    month: '$id',
+                },
+            },
+            {
+                $project: {
+                    _id: 0,
+                },
+            },
+            {
+                $sort: {
+                    numOfToursStarts: 1,
+                },
+            },
+        ]);
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                plan,
+            },
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'failure',
+            message: err,
+        });
+    }
+};
