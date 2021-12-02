24752ab4d9a587fc2f6f5871adeface4c4a6e49b
James McGahn <jmcgahn@gmail.com>
2021-12-02 01:53:10 -0500

    error class,  error controller

--- Projects From Tuts/Node.js/NatourAPI/app.js
+++ Projects From Tuts/Node.js/NatourAPI/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const userRouter = require('./routes/userRoutes');
 const tourRouter = require('./routes/tourRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
@@ -18,10 +20,9 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-    res.status(404).json({
-        status: 'failure',
-        message: `${req.originalUrl} cannot be found`,
-    });
+    next(new AppError(`${req.originalUrl} cannot be found`, 404));
 });
+
+app.use(globalErrorHandler);
 
 module.exports = app;
