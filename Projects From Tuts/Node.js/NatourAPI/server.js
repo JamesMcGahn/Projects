dc6dd7e9ac55ce3f278d521f599f06de4f325b8f
James McGahn <jmcgahn@gmail.com>
2021-12-03 23:27:21 -0500

    uncaught & unhandled error handling fallbacks

--- Projects From Tuts/Node.js/NatourAPI/server.js
+++ Projects From Tuts/Node.js/NatourAPI/server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+    console.log('Unhandled Exception!');
+    console.log(err.name, err.message);
+    process.exit(1);
+});
+
 dotenv.config({ path: './.env' });
 const app = require('./app');
 
@@ -14,6 +20,14 @@ mongoose.connect(DB, {
 
 // start server
 const port = process.env.PORT;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`app firing on ${port}`);
+});
+
+process.on('unhandledRejection', err => {
+    console.log('Unhandled rejection!');
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    });
 });
