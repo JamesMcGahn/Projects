93939b500bb59727bdc99a32d9fb6d0815634e12
James McGahn <jmcgahn@gmail.com>
2021-12-05 00:41:26 -0500

    user model & auth controller

--- Projects From Tuts/Node.js/NatourAPI/routes/userRoutes.js
+++ Projects From Tuts/Node.js/NatourAPI/routes/userRoutes.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
+
+router.post('/signup', authController.signup);
 
 router.route('/')
     .get(userController.getAllUsers)
     .post(userController.createUser);
 
-// eslint-disable-next-line prettier/prettier
 router.route('/:id')
     .get(userController.getUser)
     .patch(userController.updateUser)
