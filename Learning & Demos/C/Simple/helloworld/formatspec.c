a43ab018112108249894f648311153cb5f8cfde0
James McGahn <jmcgahn@gmail.com>
2022-02-20 23:40:40 -0500

    basic vars, for loops

---
+++ Learning & Demos/C/Simple/helloworld/formatspec.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Today i want %d sandwhiches and %d cans of coke. \nIt will cost %.2f \n", 4,2,10.98);
+    printf("we have %d coins \n", 121);
+    printf("we have %f coins here \n", 12.5 );
+    printf("we have on avg %f chickens \n", 121.1212);
+    printf("add = %d and add = %d equals = %d \n", 5,5,5+5);
+    printf("num = %d and minus = %d equals = %d \n", 5,3,5-3);
+    printf("add = %f and add = %f equals = %f \n", 5.3,5.3,5.3+5.3);
+    return 0;
+}
---
+++ Learning & Demos/C/Simple/varsds/vars.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1=5, num2=2, num3=4;
+    double result;
+    result = (num1+(double)num2+num3)/3;
+    printf("%f \n", result);
+    return 0;
+}
