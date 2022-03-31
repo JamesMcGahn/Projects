66adc3f462d39395bb4881edee719ce86f7e84d5
James McGahn <jmcgahn@gmail.com>
2022-03-31 00:13:23 -0400

    ts webframework demo

--- Learning & Demos/Typescript/Demo/WebFramework/src/index.ts
+++ Learning & Demos/Typescript/Demo/WebFramework/src/index.ts
@@ -1,14 +1,17 @@
-import axios from 'axios';
-import { User, UserProps } from './models/User';
-
-// const user = User.buildUser({ id: 2 });
-// user.on('change', () => {
-//   console.log(user);
-// });
-
+import { UserList } from './views/UserList';
 import { Collection } from './models/Collection';
-const collection = User.buildUserCollection();
-collection.on('change', () => {
-  console.log(collection);
+import { UserProps, User } from './models/User';
+
+const users = new Collection('http://localhost:3000/users', (json: UserProps) => {
+  return User.buildUser(json);
 });
-collection.fetch();
+
+users.on('change', () => {
+  const root = document.getElementById('root');
+
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();
