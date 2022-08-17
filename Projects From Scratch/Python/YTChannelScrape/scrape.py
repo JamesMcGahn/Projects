dc6022895f6a3726a2befe799c2a2662d3569edf
James McGahn <jmcgahn@gmail.com>
2022-08-17 13:37:31 -0400

    err handling for webdriver 404, write to csv, html writing into fn

--- Projects From Scratch/Python/YTChannelScrape/scrape.py
+++ Projects From Scratch/Python/YTChannelScrape/scrape.py
@@ -1,5 +1,6 @@
 import os
 from time import sleep
+from csv import DictWriter
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -12,42 +13,76 @@ def run_webdriver(channel_name, filename):
     sleep(3)
 
     last_scroll_height = 0
-    current_height = driver.execute_script(
+    try:
+        current_height = driver.execute_script(
         "return document.getElementById('content').scrollHeight"
     )
 
-    while last_scroll_height != current_height:
-        driver.execute_script(
-            "window.scrollTo(0, document.getElementById('content').scrollHeight)"
-        )
-        last_scroll_height = current_height
-        sleep(5)
-        
-        current_height = driver.execute_script(
-            "return document.getElementById('content').scrollHeight"
-        )
-        
-        print(last_scroll_height)
-        print(current_height)
+        while last_scroll_height != current_height:
+            driver.execute_script(
+                "window.scrollTo(0, document.getElementById('content').scrollHeight)"
+            )
+            last_scroll_height = current_height
+            sleep(5)
+            
+            current_height = driver.execute_script(
+                "return document.getElementById('content').scrollHeight"
+            )
+            
+            print(last_scroll_height)
+            print(current_height)
+            page_source = driver.page_source
+            driver.close()
+            print('made it here')
+            return page_source
+    except:
+        if '404' in driver.title:
+            print('Cannot find that youtube channel. Please check the url structure or channel name')
+        else:
+            print('An error has occurred')
 
+def write_html_file(filename, source):
     html_file = f"{filename}.html"
     file = open(html_file, "w")
-    file.write(driver.page_source)
+    file.write(source)
     file.close()
-    driver.close()
+    
 
 
-run_webdriver(
-    "channel",
+# videos and links
+def scrape_html(filename):
+    data = open(f"./{filename}", "r")
+    soup = BeautifulSoup(data, "html.parser")
+    all_videos = soup.find_all("ytd-grid-video-renderer")
+    video_collection = []
+
+    for video in all_videos:
+        vid = {
+            "Title": video.find("h3").get_text(),
+            "Video Url": f"www.youtube.com/{video.find(id='thumbnail')['href']}"
+        }
+        video_collection.append(vid)
+    try:
+        os.remove(filename)
+    except OSError as error:
+        print(error)
+        print("File can not be removed")
+    
+    return video_collection
+    
+
+
+def write_to_file(collection):
+    with open("videos.csv", "w") as file:
+        headers=["Title", "Video Url"]
+        csv_writer = DictWriter(file, fieldnames=headers)
+        csv_writer.writeheader()
+        for video in collection:
+            print(video)
+            csv_writer.writerow(video)
+
+videos = run_webdriver(
+    "112222",
     "dss",
 )
 
-# videos and links
-data = open("./dss.html", "r")
-soup = BeautifulSoup(data, "html.parser")
-all_videos = soup.find_all("ytd-grid-video-renderer")
-for video in all_videos:
-    print(video.find("h3").get_text())
-    print(video.find(id="thumbnail")["href"])
-
-os.remove("./dss.html")
