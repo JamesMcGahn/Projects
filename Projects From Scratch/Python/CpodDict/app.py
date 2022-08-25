9942d4159fea581f76983aa6f6bbd163544ed5ce
James McGahn <jmcgahn@gmail.com>
2022-08-25 16:43:56 -0400

    add idecontroller, refact mdgb_scrape to use idecontroller, add idecontroller to start

--- Projects From Scratch/Python/CpodDict/app.py
+++ Projects From Scratch/Python/CpodDict/app.py
@@ -1,5 +1,6 @@
 from cpod_scrape import ScrapeCpod
 from dictionary import Dictionary
+from idecontroller import IDEController
 from keys import keys
 from mdgb_scrape import ScrapeMdgb
 from open_file import OpenFile
@@ -16,15 +17,26 @@ from write_file import WriteFile
 
 def start(filepath):
     try:
+        print("Where is the file to open?")
+        definition_options = ["Use Cpod Definitions", "Use Mdgb Definitions"]
+
+        IDEController.list_options(
+            definition_options,
+            "Where should we grab defintions from?",
+            lambda x, count: print(f"{count}:{x}"),
+        )
+        definition_source = IDEController.make_selection(
+                definition_options, "Enter the number of the source you want to use"
+            )
         words_list = OpenFile.open_word_list(filepath, "\n")
         new_session = Session(
             f"{keys['url']}accounts/signin", keys["email"], keys["password"]
         )
         new_session.get_session()
-
         dictionary = Dictionary()
 
         levels = ("Elementary", "Pre-Intermediate", "Intermediate")
+
         for word in words_list:
             c_soup_res = new_session.get_html(
                 f"{keys['url']}/dictionary/english-chinese/", word
@@ -32,14 +44,17 @@ def start(filepath):
             cpod = ScrapeCpod(c_soup_res, word)
             c_defined_word = cpod.get_defintion()
             example_sentences = cpod.get_sentences()
-            m_soup_res = new_session.get_html(
+            if definition_source == 0:
+                dictionary.add_word(c_defined_word)
+            else:
+                m_soup_res = new_session.get_html(
                 "https://www.mdbg.net/chinese/dictionary?page=worddict&wdrst=0&wdqtm=0&wdqcham=1&wdqt=",
                 word,
             )
-            mdgb = ScrapeMdgb(m_soup_res, word)
-            m_defined_word = mdgb.get_defintion()
-            m_defined_word.audio = c_defined_word.audio
-            dictionary.add_word(m_defined_word)
+                mdgb = ScrapeMdgb(m_soup_res, word)
+                m_defined_word = mdgb.get_defintion()
+                m_defined_word.audio = c_defined_word.audio
+                dictionary.add_word(m_defined_word)
             dictionary.add_sentences(example_sentences)
 
         WriteFile.write_to_csv(dictionary.get_words(), "words")
--- Projects From Scratch/Python/CpodDict/mdgb_scrape.py
+++ Projects From Scratch/Python/CpodDict/mdgb_scrape.py
@@ -1,4 +1,5 @@
 from dictionary import Word
+from idecontroller import IDEController
 
 
 class ScrapeMdgb:
@@ -9,25 +10,17 @@ class ScrapeMdgb:
 
     def def_selection(self):
         if len(self.result_words) > 1:
-            count = 1
-            print("There are mulitple results:")
-            for word in self.result_words:
-                print(
-                    f"{count}:{word['chinese']}({word['pinyin']}) - {word['definition']}"
-                )
-                count += 1
-            selection = None
-            while selection not in range(1, len(self.result_words) + 1):
-                try:
-                    selection = int(input("Enter the number the correct definition: "))
-                    if selection not in range(1, len(self.result_words) + 1):
-                        print(
-                            "Thats not a valid selection. Enter a number listed above."
-                        )
-                except ValueError:
-                    print("What you entered is not a number. Please try again.")
-                    continue
-            sel_word = self.result_words[selection - 1]
+            IDEController.list_options(
+                self.result_words,
+                "There are multiple results:",
+                lambda x, count: print(
+                    f"{count}:{x['chinese']}({x['pinyin']}) - {x['definition']}"
+                ),
+            )
+            selection = IDEController.make_selection(
+                self.result_words, "Enter the number the correct definition"
+            )
+            sel_word = self.result_words[selection]
             return Word(
                 sel_word["chinese"],
                 sel_word["pinyin"],
