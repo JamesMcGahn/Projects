bb4cbc2465b0b00be7035ac5f76f38585e96b8fe
James McGahn <jmcgahn@gmail.com>
2022-08-26 17:00:46 -0400

    add dialog scrap, remove word in constructor, restructure class

--- Projects From Scratch/Python/CpodDict/cpod_scrape.py
+++ Projects From Scratch/Python/CpodDict/cpod_scrape.py
@@ -5,11 +5,22 @@ from dictionary import Dictionary, Sentence, Word
 
 
 class ScrapeCpod:
-    def __init__(self, soup, word):
+    def __init__(self, soup):
         self.soup = soup
-        self.word = word
+        self.word_example_sentences = []
+        self.dialogue = []
+        self.definition = None
 
-    def get_audio(self, element):
+    def get_sentences(self):
+        return self.word_example_sentences
+
+    def get_defintion(self):
+        return self.definition
+
+    def get_dialogues(self):
+        return self.dialogue
+
+    def scrape_audio(self, element):
         audio_table = element.find(class_="jp-type-single")
         if audio_table is None:
             return ""
@@ -17,19 +28,20 @@ class ScrapeCpod:
         audio_file = audio_file.replace("/redirect/?url=", "")
         return audio_file
 
-    def get_defintion(self):
-        print(f"Getting Definition for {self.word} - cpod")
+    def scrape_defintion(self):
         search_table = self.soup.find("div", class_="sample-search")
         if search_table is None:
             return None
 
-        pinyin_def = self.soup.find("div", class_="sample-search")
-        audio_file = self.get_audio(search_table)
+        audio_file = ""
+        word = ""
         pinyin = ""
         definition = ""
 
-        if pinyin_def is not None:
-            pinyin_def = pinyin_def.find("p").get_text()
+        if search_table is not None:
+            word = Dictionary.strip_string(search_table.find("h2").next_element)
+            audio_file = self.scrape_audio(search_table)
+            pinyin_def = search_table.find("p").get_text()
             pinyin_def = (
                 pinyin_def.replace("Pinyin: ", "")
                 .replace("Definition: ", "")
@@ -38,11 +50,11 @@ class ScrapeCpod:
             pinyin = pinyin_def[0]
             definition = pinyin_def[1]
 
-        return Word(self.word, pinyin, definition, audio_file)
+        self.definition = Word(word, pinyin, definition, audio_file)
 
-    def get_sentences(self):
+    def scrape_sentences(self):
         reg_pattern = regex.compile(r"[\p{Han}，。？：！‘\"\\s]+")
-        word_example_sentences = []
+
         sample_sentences_table = self.soup.find("table", class_="table-grossary")
         if sample_sentences_table is None:
             return []
@@ -55,11 +67,28 @@ class ScrapeCpod:
             char_sent = reg_pattern.findall(sentence_all)[0]
             pinyin_sent = sent_cont.find(class_="dict-pinyin-cont").get_text()
             english_sent = sent_cont.find(class_="dict-pinyin-cont").next_sibling
-            audio = self.get_audio(sentence)
+            audio = self.scrape_audio(sentence)
             english_sent = Dictionary.strip_string(english_sent)
             pinyin_sent = Dictionary.strip_string(pinyin_sent)
             example_sentence = Sentence(
                 self.word, char_sent, english_sent, pinyin_sent, level, audio
             )
-            word_example_sentences.append(example_sentence)
-        return word_example_sentences
+            self.word_example_sentences.append(example_sentence)
+
+    def scrape_dialogues(self):
+        dialogue_cont = self.soup.find("div", id="dialogue")
+        dialogue = dialogue_cont.find_all("tr")
+        title_cont = self.soup.find("h1", class_="lesson-page-title")
+        title = title_cont.find("span", attrs={"itemprop": "name"}).string
+        badge = title_cont.find("a", class_="badge").get_text()
+        for sentence in dialogue:
+
+            chinese = sentence.find("p", class_="click-to-add").get_text()
+            pinyin = sentence.find("p", class_="show-pinyin").get_text()
+            english = sentence.find("p", class_="translation-container").get_text()
+            audio = self.scrape_audio(sentence)
+            chinese = Dictionary.strip_string(chinese)
+            pinyin = Dictionary.strip_string(pinyin)
+            english = Dictionary.strip_string(english)
+            dialogue_sent = Sentence(title, chinese, english, pinyin, badge, audio)
+            self.dialogue.append(dialogue_sent)
