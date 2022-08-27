71339b07b9fd8cf21252cb4681d76a7d048d76c1
James McGahn <jmcgahn@gmail.com>
2022-08-27 12:58:45 -0400

    add terminal opts class

---
+++ Projects From Scratch/Python/CpodDict/terminal_opts.py
@@ -0,0 +1,25 @@
+from simple_term_menu import TerminalMenu
+
+
+class TerminalOptions:
+    def __init__(self, options, multi):
+        self.options = options
+        self.isMulti = multi
+        self.start = TerminalMenu(
+            self.options, multi_select=self.isMulti, show_multi_select_hint=self.isMulti
+        )
+        self.indexes = self.show()
+        self.chosen = self.start.chosen_menu_entries
+
+    def show(self):
+        return self.start.show()
+
+    def get_selected(self, arry=False):
+        if not self.isMulti and arry:
+            return [self.options[self.indexes]]
+        elif not self.isMulti:
+            return self.options[self.indexes]
+        elif self.isMulti and arry:
+            return list(self.chosen)
+        else:
+            return self.chosen
