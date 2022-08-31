3fe7094c2e2c1de33c6b29d596514d9ca124474d
James McGahn <jmcgahn@gmail.com>
2022-08-30 21:11:59 -0400

    add generic write file method, fix check_dup method bug

--- Projects From Scratch/Python/CpodDict/write_file.py
+++ Projects From Scratch/Python/CpodDict/write_file.py
@@ -1,4 +1,5 @@
 import os
+import re
 from csv import DictWriter
 
 
@@ -21,28 +22,49 @@ class WriteFile:
         if isinstance(filename, int):
             filename = str(filename)
         filename = filename.replace("/", "-").replace("\0", "")
-        path = f"{folderpath}/{filename}{ext}"
+        path = f"{folderpath}{filename}{ext}"
         # check if filename exists
         if WriteFile.path_exists(path, False):
             count = 1
-            newpath = f"{folderpath}/{filename}-({count}){ext}"
+            newpath = f"{folderpath}{filename}-({count}){ext}"
             # if filename exists append -(count) to make unique filename
             # check to see if filename already exists - if it does increase count in filename
             while WriteFile.path_exists(newpath, False):
-                newpath = f"{folderpath}/{filename}-({count}){ext}"
+                newpath = f"{folderpath}{filename}-({count}){ext}"
                 count += 1
             path = newpath
         return path
 
     @staticmethod
-    def write_to_csv(data, filename):
-        
+    def write_to_csv(path, data):
         if len(data) == 0:
             return False
-        path = WriteFile.check_dup("./out", filename, ".csv")
+        match = WriteFile.regex_path(path)
+        path = WriteFile.check_dup(match["path"], match["filename"], match["ext"])
         print(f"Saving {path}")
         with open(path, "w") as file:
             csv_writer = DictWriter(file, fieldnames=data[0].keys())
             csv_writer.writeheader()
             for dat in data:
                 csv_writer.writerow(dat)
+
+    def regex_path(path):
+        path_reg = re.compile(
+            r"^(?P<path>(\.*[\/[\d\w\s-]+\/]*)|\.+\/)(?P<filename>[\d\w\s-]*)(?P<ext>\.[\w]+)$"
+        )
+        matches = path_reg.search(path)
+        return {
+            "path": matches.group("path"),
+            "filename": matches.group("filename"),
+            "ext": matches.group("ext"),
+        }
+
+    @staticmethod
+    def write_file(path, source, write_type="w"):
+        print("Writing Data to File")
+        match = WriteFile.regex_path(path)
+        path = WriteFile.check_dup(match["path"], match["filename"], match["ext"])
+        with open(path, write_type) as out:
+            out.write(source)
+            print(f"Completed Writing {match['filename']}{match['ext']}")
+        return path
