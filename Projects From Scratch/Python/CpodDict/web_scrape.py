ef1f351f310b9e19b01662f343eed4347f6ebf50
James McGahn <jmcgahn@gmail.com>
2022-09-24 12:03:20 -0400

    FIX: exclude cookies from other chinese dict source domain, add Logger
    - remove cookies from other chinese dict source so webdriver doesnt throw error
    - add Logger

--- Projects From Scratch/Python/CpodDict/web_scrape.py
+++ Projects From Scratch/Python/CpodDict/web_scrape.py
@@ -1,5 +1,7 @@
 from time import sleep
 
+from keys import keys
+from logger import Logger
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.service import Service
@@ -18,27 +20,34 @@ class WebScrape:
     def get_source(self):
         return {"source": self.source, "not_available": self.not_available}
 
+    def close(self):
+        self.driver.close()
+
     def init_driver(self):
         cookies = self.cookies
         url = self.session.get_session_url()
+        sleep(3)
         try:
             self.driver.get(url)
             for c in cookies:
-                self.driver.add_cookie(
-                    {
-                        "name": c.name,
-                        "value": c.value,
-                        "domain": c.domain,
-                        "path": c.path,
-                    }
-                )
+
+                if c.domain not in keys["mdomain"]:
+                    self.driver.add_cookie(
+                        {
+                            "name": c.name,
+                            "value": c.value,
+                            "domain": c.domain,
+                            "path": c.path,
+                        }
+                    )
 
         except Exception as e:
-            print(e)
+            Logger().insert("Failed loading cookies", "ERROR")
+            Logger().insert(e, "ERROR", False)
 
     def run_webdriver(self, url):
         try:
-            print(f"Starting Scrape......{url}")
+            Logger().insert(f"Starting Scrape......{url}", "INFO")
             self.not_available = []
             self.driver.get(url)
             sleep(3)
@@ -50,11 +59,12 @@ class WebScrape:
                     sleep(3)
                 except NoSuchElementException:
                     self.not_available.append(link)
-                    print(f"Lesson doesn't have a {link} section")
+                    Logger().insert(f"Lesson doesn't have a {link} section", "WARN")
 
             page_source = self.driver.page_source
-            print("Completed Scrape .....")
+            Logger().insert("Completed Scrape .....", "INFO")
             self.source = page_source
         except Exception as e:
-            print(e)
+            Logger().insert("Something Went Wrong...", "ERROR")
+            Logger().insert(e, "ERROR", False)
             raise RuntimeError("An error has occurred")
