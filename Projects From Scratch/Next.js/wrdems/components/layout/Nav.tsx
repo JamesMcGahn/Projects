11d0ace63b18860bd892c5cb6d9980f17c2d5992
James McGahn <jmcgahn@gmail.com>
2022-10-05 18:19:39 -0400

    add fontawesome, style nav

--- Projects From Scratch/Next.js/wrdems/components/layout/Nav.tsx
+++ Projects From Scratch/Next.js/wrdems/components/layout/Nav.tsx
@@ -4,33 +4,35 @@ import Navbar from 'react-bootstrap/Navbar';
 import classes from '../../styles/Nav.module.css';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquareFacebook } from '@fortawesome/free-brands-svg-icons';
 
 function MainNav() {
   const router = useRouter();
   return (
     <Container className={classes.container} fluid>
-      <Navbar id={classes.nav} bg="dark" expand="lg" variant="dark" fixed="top">
+      <Navbar id={classes.nav} bg="dark" expand="lg" variant="dark">
         <Container id={classes.innerCont}>
+          <Link href="/" passHref>
+            <Navbar.Brand href="#home">Wood-Ridge Democrats</Navbar.Brand>
+          </Link>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
             <Nav className="ml-auto">
               <Link href="/" passHref>
                 <Nav.Link>Home</Nav.Link>
               </Link>
-              <Link href="/#about" passHref>
-                <Nav.Link>About</Nav.Link>
+              <Link href="/#about-michele" passHref>
+                <Nav.Link>About Michele</Nav.Link>
               </Link>
-              <Link href="/" passHref>
-                <Nav.Link>Link 3</Nav.Link>
-              </Link>
-              <Link href="/" passHref>
-                <Nav.Link>Link 4</Nav.Link>
-              </Link>
-              <Link href="/" passHref>
-                <Nav.Link>Link 5</Nav.Link>
+              <Link href="/#about-mike" passHref>
+                <Nav.Link>About Mike</Nav.Link>
               </Link>
             </Nav>
           </Navbar.Collapse>
+          <a href="https://www.facebook.com/WRDems" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faSquareFacebook} />
+          </a>
         </Container>
       </Navbar>
     </Container>
--- Projects From Scratch/Next.js/wrdems/pages/_app.tsx
+++ Projects From Scratch/Next.js/wrdems/pages/_app.tsx
@@ -1,5 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
