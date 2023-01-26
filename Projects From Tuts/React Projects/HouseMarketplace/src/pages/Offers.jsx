fc730cbfa78873a9f21f41eef87eeb284445a9fc
James McGahn <jmcgahn@gmail.com>
2023-01-25 21:58:25 -0500

    offers page

--- Projects From Tuts/React Projects/HouseMarketplace/src/pages/Offers.jsx
+++ Projects From Tuts/React Projects/HouseMarketplace/src/pages/Offers.jsx
@@ -1,4 +1,57 @@
+import { useEffect, useState } from 'react';
+import { collection, getDocs, query, where, orderBy, limit } from 'firebase/firestore';
+import { db } from '../firebase.config';
+import { toast } from 'react-toastify';
+import Spinner from '../components/Spinner';
+import ListingItem from '../components/ListingItem';
+
 function Offers() {
-  return <div>Offers</div>;
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        const listingRef = collection(db, 'listings');
+        const q = query(listingRef, where('offer', '==', true), orderBy('timestamp', 'desc'), limit(10));
+        const querySnap = await getDocs(q);
+
+        let listings = [];
+        querySnap.forEach((doc) => {
+          return listings.push({ id: doc.id, data: doc.data() });
+        });
+
+        setListings(listings);
+        setLoading(false);
+      } catch {
+        toast.error('Could not fetch listings');
+      }
+    };
+    fetchListings();
+  }, []);
+
+  return (
+    <div className="category">
+      <header>
+        <p className="pageHeader">Offers</p>
+      </header>
+
+      {loading ? (
+        <Spinner />
+      ) : listings && listings.length > 0 ? (
+        <>
+          <main>
+            <ul className="categoryListings">
+              {listings.map((listing) => (
+                <ListingItem listing={listing.data} id={listing.id} key={listing.id} />
+              ))}
+            </ul>
+          </main>
+        </>
+      ) : (
+        <p>No Current Offers</p>
+      )}
+    </div>
+  );
 }
 export default Offers;
