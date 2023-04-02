8ba95db9be36fa432896e9c7af5adb615d451f47
almag3st <jmcgahn@gmail.com>
2023-04-02 09:33:01 -0400

    authslice, authserive - register, home page

--- Projects From Tuts/Full Stack Projects/SupportDesk/frontend/src/pages/Register.jsx
+++ Projects From Tuts/Full Stack Projects/SupportDesk/frontend/src/pages/Register.jsx
@@ -1,6 +1,9 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FaUser } from 'react-icons/fa';
 import { toast } from 'react-toastify';
+import { useSelector, useDispatch } from 'react-redux';
+import { register, reset } from '../features/auth/authSlice';
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -11,6 +14,21 @@ function Register() {
   });
 
   const { name, email, password, password2 } = formData;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+    console.log(message);
+    if (isSuccess && user) {
+      navigate('/');
+    }
+    dispatch(reset());
+  }, [isError, isSuccess, user, message, navigate, dispatch]);
 
   const onChange = (e) => {
     setFormData((prevState) => ({ ...prevState, [e.target.name]: e.target.value }));
@@ -20,6 +38,13 @@ function Register() {
     e.preventDefault();
     if (password !== password2) {
       toast.error('Passwords do not match');
+    } else {
+      const userData = {
+        name,
+        email,
+        password,
+      };
+      dispatch(register(userData));
     }
   };
 
