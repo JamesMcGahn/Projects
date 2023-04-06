73eb0f7f139b531e902db120b48ffab07b6bc14d
James McGahn <jmcgahn@gmail.com>
2023-04-05 22:51:25 -0400

    add note slice and note service

---
+++ Projects From Tuts/Full Stack Projects/SupportDesk/frontend/src/features/notes/noteService.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const API_URL = '/api/tickets/';
+
+const getNotes = async (ticketId, token) => {
+  const config = {
+    headers: { Authorization: `Bearer ${token}` },
+  };
+
+  const response = await axios.get(`${API_URL}${ticketId}/notes`, config);
+  return response.data;
+};
+
+const createNote = async (text, ticketId, token) => {
+  const config = {
+    headers: { Authorization: `Bearer ${token}` },
+  };
+
+  const response = await axios.post(`${API_URL}${ticketId}/notes`, { text }, config);
+
+  return response.data;
+};
+
+const noteService = {
+  getNotes,
+  createNote,
+};
+
+export default noteService;
