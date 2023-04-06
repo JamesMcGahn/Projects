816cfc3929e35d74a71cf6715b715d47a0f6d5f3
James McGahn <jmcgahn@gmail.com>
2023-04-05 22:51:58 -0400

    add note item and note modal to ticket, add ability to post new note

--- Projects From Tuts/Full Stack Projects/SupportDesk/frontend/src/pages/Ticket.jsx
+++ Projects From Tuts/Full Stack Projects/SupportDesk/frontend/src/pages/Ticket.jsx
@@ -1,30 +1,76 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Spinner from '../components/Spinner';
 import BackButton from '../components/BackButton';
+import NoteItem from '../components/NoteItem';
+import Modal from 'react-modal';
 import { toast } from 'react-toastify';
 import { getTicket, closeTicket, reset } from '../features/tickets/ticketSlice';
+import { getNotes, createNote, reset as noteReset } from '../features/notes/noteSlice';
 import { useParams, useNavigate } from 'react-router-dom';
+import { FaPlus } from 'react-icons/fa';
+
+const customStyles = {
+  content: {
+    width: '600px',
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    position: 'relative',
+  },
+};
+
+Modal.setAppElement('#root');
 
 function Ticket() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [noteText, setNoteText] = useState('');
   const { ticket, isLoading, isSuccess } = useSelector((state) => state.tickets);
+  const { notes, isLoading: notesIsLoading, isSuccess: notesSuccess } = useSelector((state) => state.notes);
   const dispatch = useDispatch();
-  const params = useParams();
+  const { ticketId } = useParams();
   const navigate = useNavigate();
 
   useEffect(() => {
-    dispatch(getTicket(params.ticketId));
+    dispatch(getTicket(ticketId));
+    dispatch(getNotes(ticketId));
     return () => {
       if (isSuccess) {
         reset();
       }
+      if (notesSuccess) {
+        noteReset();
+      }
     };
-  }, [dispatch, isSuccess, params.ticketId]);
+  }, [dispatch, isSuccess, ticketId, notesSuccess]);
 
   const onCloseTicket = () => {
-    dispatch(closeTicket(params.ticketId));
-    toast.success('Ticket Closed');
-    navigate('/tickets');
+    dispatch(closeTicket(ticketId))
+      .unwrap()
+      .then(() => {
+        toast.success('Ticket Closed');
+        navigate('/tickets');
+      })
+      .catch(toast.error);
+  };
+
+  const openModal = () => setModalIsOpen(true);
+  const closeModal = () => setModalIsOpen(false);
+
+  const onNoteSubmit = (e) => {
+    // NOTE: we can unwrap our AsyncThunkACtion here so no need for isError and
+    // isSuccess state
+    e.preventDefault();
+    dispatch(createNote({ noteText, ticketId }))
+      .unwrap()
+      .then(() => {
+        setNoteText('');
+        closeModal();
+      })
+      .catch(toast.error);
   };
 
   if (isLoading) {
@@ -46,6 +92,39 @@ function Ticket() {
           <p>{ticket.description}</p>
         </div>
       </header>
+
+      {ticket.status !== 'closed' && (
+        <button onClick={openModal} className="btn">
+          <FaPlus /> Add Note
+        </button>
+      )}
+
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} style={customStyles} contentLabel="Add Note">
+        <h2>Add Note</h2>
+        <button className="btn-close" onClick={closeModal}>
+          X
+        </button>
+        <form onSubmit={onNoteSubmit}>
+          <div className="form-group">
+            <textarea
+              name="noteText"
+              id="noteText"
+              className="form-control"
+              placeholder="Note text"
+              value={noteText}
+              onChange={(e) => setNoteText(e.target.value)}
+            ></textarea>
+          </div>
+          <div className="form-group">
+            <button className="btn" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+      </Modal>
+
+      {!notesIsLoading && notes.map((note) => <NoteItem key={note._id} note={note} />)}
+
       {ticket.status !== 'closed' && (
         <button onClick={onCloseTicket} className="btn btn-block btn-danger">
           Close Ticket
