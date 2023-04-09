7177dd5d900e00ab12eba872dacf85b784feb509
almag3st <jmcgahn@gmail.com>
2023-04-09 19:39:23 -0400

    tour, user controllers

---
+++ Projects From Tuts/Node.js/NatourAPI/controllers/tourController.js
@@ -0,0 +1,95 @@
+const Tour = require('../models/toursModel');
+
+exports.checkID = (req, res, next, val) => {
+  // console.log(`Tour id is: ${val}`);
+
+  // if (req.params.id * 1 > tours.length) {
+  //   return res.status(404).json({
+  //     status: 'fail',
+  //     message: 'Invalid ID',
+  //   });
+  // }
+  next();
+};
+
+exports.getAllTours = async (req, res) => {
+  const tours = await Tour.find();
+
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (e) {
+    res.status(404).json({
+      status: 'failure',
+      message: e,
+    });
+  }
+};
+
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (e) {
+    res.status(400).json({
+      status: 'failure',
+      message: e,
+    });
+  }
+};
+
+exports.updateTour = async (req, res) => {
+  try {
+    const updatedTour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: updatedTour,
+      },
+    });
+  } catch (e) {
+    res.status(400).json({
+      status: 'failure',
+      message: e,
+    });
+  }
+};
+
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (e) {
+    res.status(400).json({
+      status: 'failure',
+      message: e,
+    });
+  }
+};
