2ce9898f4f6776ac48b21fac4e88212e95095aa4
almag3st <jmcgahn@gmail.com>
2023-04-25 00:54:34 -0400

    add global error handler controller, error class

--- Projects From Tuts/Node.js/NatourAPI/app.js
+++ Projects From Tuts/Node.js/NatourAPI/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-
+const AppError = require('./utils/appError');
+const globalErrHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -19,5 +20,11 @@ app.use(express.static(`${__dirname}/public`));
 //  ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Cant find ${req.originalUrl}`, 404));
+});
+
+app.use(globalErrHandler);
 
 module.exports = app;
