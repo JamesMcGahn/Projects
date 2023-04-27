9cb80dcadea673a4c8ff5fbd39de048468fe54a1
almag3st <jmcgahn@gmail.com>
2023-04-27 01:45:28 -0400

    add error controller

--- Projects From Tuts/Node.js/NatourAPI/controllers/errorController.js
+++ Projects From Tuts/Node.js/NatourAPI/controllers/errorController.js
@@ -1,9 +1,35 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+    error: err,
+  });
+};
+const sendErrorProd = (err, res) => {
+  // known operational error
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // unknown error
+    console.error('ERROR', err);
+    res.status(err.statusCode).json({
+      status: 'error',
+      message: 'Something went very wrong',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  } else if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  }
 };
