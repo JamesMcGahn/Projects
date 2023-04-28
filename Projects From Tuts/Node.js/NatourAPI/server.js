c8acbd8cd99652be98543f881edda9e9617f1242
almag3st <jmcgahn@gmail.com>
2023-04-27 23:41:43 -0400

    unhandled expections, rejections

--- Projects From Tuts/Node.js/NatourAPI/server.js
+++ Projects From Tuts/Node.js/NatourAPI/server.js
@@ -2,11 +2,23 @@ const dotenv = require('dotenv');
 const app = require('./app');
 const connectDB = require('./config/db');
 
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './.env' });
 
 connectDB();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
