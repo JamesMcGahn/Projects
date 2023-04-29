28f50303fb3faff2a54cafd9b31514a0043665fc
almag3st <jmcgahn@gmail.com>
2023-04-29 02:56:41 -0400

    add pw hash pre save

--- Projects From Tuts/Node.js/NatourAPI/models/userModel.js
+++ Projects From Tuts/Node.js/NatourAPI/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -24,7 +25,21 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Confirm your password'],
+    validate: {
+      validator: function (pw) {
+        return pw === this.password;
+      },
+      message: 'Passwords do not match',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
