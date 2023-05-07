bb6d12958b17cd572d75225a9de8beeb0990a46f
almag3st <jmcgahn@gmail.com>
2023-05-07 19:31:16 -0400

    restrict route mw to roles

--- Projects From Tuts/Node.js/NatourAPI/controllers/authController.js
+++ Projects From Tuts/Node.js/NatourAPI/controllers/authController.js
@@ -75,3 +75,14 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = isUserActive;
   next();
 });
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have permission to perform this action', 403),
+      );
+    }
+    next();
+  };
+};
