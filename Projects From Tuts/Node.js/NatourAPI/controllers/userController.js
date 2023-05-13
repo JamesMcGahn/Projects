37edbe1dcd19061cdec4c478d1e503bb1e9e7ace
almag3st <jmcgahn@gmail.com>
2023-05-13 16:52:14 -0400

    updateMe - update user route and handler

--- Projects From Tuts/Node.js/NatourAPI/controllers/userController.js
+++ Projects From Tuts/Node.js/NatourAPI/controllers/userController.js
@@ -1,5 +1,16 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObject = {};
+  Object.keys(obj).forEach((key) => {
+    if (allowedFields.includes(key)) {
+      newObject[key] = obj[key];
+    }
+  });
+  return newObject;
+};
 
 exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
@@ -33,3 +44,23 @@ exports.deleteUser = (req, res) => {
     message: 'This route is not yet defined!',
   });
 };
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  const { password, passwordConfirm } = req.body;
+
+  if (password || passwordConfirm) {
+    next(new AppError('Updating Password is not allowed on this Route.', 400));
+  }
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
+});
