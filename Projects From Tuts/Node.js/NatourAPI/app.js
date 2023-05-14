6655b237ad40507a6a9f9a5178e482da3969bd91
almag3st <jmcgahn@gmail.com>
2023-05-13 21:56:45 -0400

    add helmet

--- Projects From Tuts/Node.js/NatourAPI/app.js
+++ Projects From Tuts/Node.js/NatourAPI/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const AppError = require('./utils/appError');
 const globalErrHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -8,12 +9,16 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // MIDDLEWARE
+// dev logging
 if (process.env.NODE_ENV === 'development') {
   // trunk-ignore(eslint/global-require)
   // trunk-ignore(eslint/node/no-unpublished-require)
   const morgan = require('morgan');
   app.use(morgan('dev'));
 }
+
+// security
+app.use(helmet());
 
 const limiter = rateLimit({
   max: 100,
@@ -23,17 +28,23 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
-app.use(express.json());
+// body parser - reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+app.use(express.static(`${__dirname}/public`));
+
+// Serving Static Files
 app.use(express.static(`${__dirname}/public`));
 
 //  ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// 404 for bad route requests
 app.all('*', (req, res, next) => {
   next(new AppError(`Cant find ${req.originalUrl}`, 404));
 });
 
+// Global Error Handler
 app.use(globalErrHandler);
 
 module.exports = app;
