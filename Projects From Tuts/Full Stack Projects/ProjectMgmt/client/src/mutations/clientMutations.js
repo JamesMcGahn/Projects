fc26c8a72b16ce664eb1c03d1734e846d90b7dbb
almag3st <jmcgahn@gmail.com>
2023-06-10 13:45:30 -0400

    client - header, client queries, display clients

---
+++ Projects From Tuts/Full Stack Projects/ProjectMgmt/client/src/mutations/clientMutations.js
@@ -0,0 +1,25 @@
+import { gql } from '@apollo/client';
+
+const DELETE_CLIENT = gql`
+  mutation deleteClient($id: ID!) {
+    deleteClient(id: $id) {
+      id
+      name
+      email
+      phone
+    }
+  }
+`;
+
+const ADD_CLIENT = gql`
+    mutation addClient(name: String!, phone: String!, email: String!){
+        addClient(name: $name, phone: $phone, email: $email)
+        {
+            id
+            name
+            email
+            phone
+        }
+    }`;
+
+export { DELETE_CLIENT, ADD_CLIENT };
