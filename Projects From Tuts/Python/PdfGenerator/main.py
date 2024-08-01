db441dc5619ce773dfd70fc16e7e72f98726ef13
James McGahn <jmcgahn@gmail.com>
2024-08-01 12:21:41 -0400

    py pdfgenerator app

---
+++ Projects From Tuts/Python/PdfGenerator/main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from fpdf import FPDF
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
+df = pd.read_csv("topics.csv")
+
+
+for _, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+    pdf.line(10, 20, 200, 20)
+
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R", ln=1)
+
+    for _ in range(row["Pages"] - 1):
+        pdf.add_page()
+        pdf.ln(275)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(100, 100, 100)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R", ln=1)
+pdf.output("output.pdf")
