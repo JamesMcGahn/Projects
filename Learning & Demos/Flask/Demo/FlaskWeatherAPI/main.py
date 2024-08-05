1dc7b737eea2da63c27dd0e51ecfbb65e8bfe41b
James McGahn <jmcgahn@gmail.com>
2024-08-04 21:47:22 -0400

    py flask web api

--- Learning & Demos/Flask/Demo/FlaskWeatherAPI/main.py
+++ Learning & Demos/Flask/Demo/FlaskWeatherAPI/main.py
@@ -1,16 +1,42 @@
+import pandas as pd
 from flask import Flask, render_template
 
-app = Flask("Website")
+app = Flask("__name__")
+
+stations = pd.read_csv("data_small/stations.txt", skiprows=17)
+print(stations.columns)
+stations = stations[["STAID", "STANAME                                 "]][0:5]
 
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data=stations.to_html())
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route("/api/v1/<station>/<date>")
+def stationdate(station, date):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+
+    temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze() / 10
+    return {"station": station, "date": date, "temperature": temperature}
 
 
-app.run(debug=True)
+@app.route("/api/v1/<station>")
+def station(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    return df.to_dict(orient="records")
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def station_yearly(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))]
+    return result.to_dict(orient="records")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
