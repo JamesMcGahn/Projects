4a2a2e413101bbd7255b3e30d8659d6151d64d6a
James McGahn <jmcgahn@gmail.com>
2026-02-22 10:48:26 -0500

    spring boot learning - 01 overview section

---
+++ Learning & Demos/SpringBoot/demos_2/01-spring-boot-overview/06-custom-application-properties/src/main/java/com/example/demo/rest/FunRestController.java
@@ -0,0 +1,32 @@
+package com.example.demo.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k.  ";
+    }
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " +  coachName + ", Team: " + teamName;
+    }
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World";
+    }
+
+
+
+
+}
